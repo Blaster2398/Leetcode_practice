90f570f2821ef1fcba569054260469453b1a465c
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-05-25 12:44:28 +0530

    Time: 2 ms (68.40%) | Memory: 14.6 MB (53.44%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
-        for(auto &i: nums){
-            cout << i << " ";
+        //step1: find the break point start from last and take a ind =-1 if there is no break point then we just have to reverse the string
+        //step2: if ind == -1 then its the largest permutation and just return the reverse 
+        //step3: now find the closest and larghest element in the right side of brreak point and swap the elements 
+        //step4: reverse the elements in the right side of the break point
+
+        int index = -1;
+        for(int i = nums.size()-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
         }
-        
+
+        if(index == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        // considering all elements in the right part are arranged in descending order 
+        for(int i = nums.size()-1; i > index; i--){
+            if(nums[index] < nums[i]){
+                swap(nums[index], nums[i]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+index+1, nums.end());
     }
 };
