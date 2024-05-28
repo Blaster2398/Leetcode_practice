21b500311c224a27450cfc8aa44ba387c8c857a4
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-05-28 22:38:12 +0530

    Time: 3 ms (96.44%) | Memory: 21 MB (64.45%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> s;
-        for(int i =0 ; i < nums.size(); i++){
-            s.insert(nums[i]);
+      // two pointer approach
+      int i = 0;
+      for(int j = 0; j < nums.size(); j++){
+        if(nums[i] != nums[j]){
+            nums[++i] = nums[j]; //if a unique element is found then take the front of the unique pointer i
         }
-
-        int res =s.size();
-        int i = 0;
-        for(auto it = s.begin(); it != s.end(); it++){ //to get the element just use *it
-            nums[i] = *it;
-            i++;
-        }
-
-
-        return res;
+      }
+      return i+1;
     }
+    // int removeDuplicates(vector<int>& nums) {
+    //     set<int> s;
+    //     for(int i =0 ; i < nums.size(); i++){
+    //         s.insert(nums[i]);
+    //     }
+
+    //     int res =s.size();
+    //     int i = 0;
+    //     for(auto it = s.begin(); it != s.end(); it++){ //to get the element just use *it
+    //         nums[i] = *it;
+    //         i++;
+    //     }
+
+
+    //     return res;
+    // }
 };
