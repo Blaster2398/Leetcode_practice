b1734a03923ad4d2be3cfdbfe15528f9f651d8f8
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-01 10:21:55 +0530

    Time: 11 ms (71.88%) | Memory: 15.9 MB (80.79%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,34 +9,85 @@
  */
 class Solution {
 public:
-    bool findPath(TreeNode* root, TreeNode* p , vector<TreeNode*> & path){
-        if(root == NULL){
-            return false;
-        }
-        path.push_back(root);
-
-        if(root->val == p->val){
-            return true;
-        }
-        if(findPath(root->left,p,path) || findPath(root->right,p,path)){
-            return true;
-        }
-        path.pop_back();
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1, path2;
-        if(findPath(root, p, path1) == false || findPath(root, q, path2) == false){
+        //if the node becomes NULL return NULL
+        if(root == NULL){
             return NULL;
         }
-        int i = 0;
-        for(i = 0; i < path1.size() && i < path2.size(); i++){
-            if(path1[i] != path2[i]){
-                break;
-            }
+        //If you find either one of the nodes you were looking for then return root
+        if(root->val == p->val || root->val == q->val){
+            return root;
         }
 
+        // if not then check for the left and right if they found anything
+        TreeNode* lca1 = lowestCommonAncestor(root->left,p,q);
+        TreeNode* lca2 = lowestCommonAncestor(root->right,p,q);
 
-        return path1[i-1];
+        // it might happen that both of them found one one node required then lca becomes the current root
+        if(lca1 != NULL && lca2 != NULL){
+            return root;
+        }
+
+        // if only one of them return something then we already have found lca and
+        //its in the one that is not NULL
+        if(lca1 != NULL){
+            return lca1;
+        }
+        else{
+            return lca2;
+        }
+    //it might happen botth are null then simply you didnt found any of them on the given path and return NULL 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // bool findPath(TreeNode* root, TreeNode* p , vector<TreeNode*> & path){
+    //     if(root == NULL){
+    //         return false;
+    //     }
+    //     path.push_back(root);
+
+    //     if(root->val == p->val){
+    //         return true;
+    //     }
+    //     if(findPath(root->left,p,path) || findPath(root->right,p,path)){
+    //         return true;
+    //     }
+    //     path.pop_back();
+    //     return false;
+    // }
+    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    //     vector<TreeNode*> path1, path2;
+    //     if(findPath(root, p, path1) == false || findPath(root, q, path2) == false){
+    //         return NULL;
+    //     }
+    //     int i = 0;
+    //     for(i = 0; i < path1.size() && i < path2.size(); i++){
+    //         if(path1[i] != path2[i]){
+    //             break;
+    //         }
+    //     }
+    //     return path1[i-1];
+    // }
+    
 };
