fbd50a481f5c54bfb583ba6470fa680865f40a13
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-06 09:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (21.31%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n == 0) {
+            return 1.0;
+        }
+
+        if(n < 0){return pow(x,n+1)/x;}
+        else{ return pow(x,n-1)*x;}
+        
     }
 };
