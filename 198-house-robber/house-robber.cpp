b866a0513fc9fd4e46d0cb8b1087bacd5c7a3811
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-12 11:04:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (59.84%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,35 +1,68 @@
 class Solution {
 public:
-    // this is a wrong code as it forces to take the last element in the solution 
-    int util(vector<int>& nums, int n, vector<int>& dp){
-        if(n==0){
-            return dp[n] = nums[0];
+    // correct implementation 
+    int util(vector<int>& nums, int ind, vector<int>& dp){
+        if(ind == 0){
+            return nums[ind];
         }
-        if(n < 0){
+        if(ind < 0){
             return 0;
         }
 
-        if(dp[n] != -1) {
-            return dp[n];
+        if(dp[ind] != -1){
+            return dp[ind];
         }
 
-        int sum = nums[n];
-        int maax = 0;
+        // here the logic is either you take or not take 
+        int not_take = 0 + util(nums, ind-1, dp);  // as this is not taken so we dont need to add anuything
+        int take = nums[ind] + util(nums, ind-2, dp); // as taken we need to leave the adjacent and add the current value 
 
-        for(int i = n-2; i>=0 ; i--){
-            int sum2 = util(nums, i,dp);
-            maax = max(maax, sum2);
-        }
-
-        return dp[n] = sum+maax;
+        return dp[ind] = max(take,not_take);
 
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp (n, -1);
-        int one  =  util(nums,n-1,dp);
-        int two =  util(nums,n-2,dp);
-
-        return  max(one,two);
+        
+        return util(nums,n-1,dp);
     }
+
+
+
+
+
+    // this is a wrong code as it forces to take the last element in the solution(so either you have to make this solution for 
+    // last and 2nd last elemnent then byh fluke it works )
+
+
+//     int util(vector<int>& nums, int n, vector<int>& dp){
+//         if(n==0){
+//             return dp[n] = nums[0];
+//         }
+//         if(n < 0){
+//             return 0;
+//         }
+
+//         if(dp[n] != -1) {
+//             return dp[n];
+//         }
+
+//         int sum = nums[n];
+//         int maax = 0;
+
+//         for(int i = n-2; i>=0 ; i--){
+//             int sum2 = util(nums, i,dp);
+//             maax = max(maax, sum2);
+//         }
+
+//         return dp[n] = sum+maax;
+
+//     }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp (n, -1);
+//         int one  =  util(nums,n-1,dp);
+//         int two =  util(nums,n-2,dp);
+//         return  max(one,two);
+//     }
 };
