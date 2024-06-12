b8a7ae7ccf6c0220d2a1b8a5662eeb83c049114d
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-12 08:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (50.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -16,8 +16,21 @@ public:
 
         return dp[n] = l+r;
     }
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     return util(n,dp);
+    // }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return util(n,dp);
+        if(n<=1) return 1;
+
+        vector<int>dp(n+1,-1);
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 };
