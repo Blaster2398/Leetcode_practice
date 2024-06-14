1c13c9b25fbe9d32583386edf429057238b6b2c2
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-14 17:27:07 +0530

    Time: 4 ms (11.94%) | Memory: 7.8 MB (16.87%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
+    // int uniquePaths(int m, int n) {
+    //     vector<int> prev(n,0);
+
+    //     for( int i = 0; i < m; i++){
+    //         vector<int> curr (n,0);
+    //         for(int j = 0; j < n; j++){
+
+    //             if(i == 0 && j == 0){
+    //                 curr[j] = 1;
+    //             }
+                
+    //             else{
+    //                 int up = (i>0) ? prev[j] :0 ;
+    //                 int left = (j>0) ? curr[j-1] : 0 ;
+    //                 curr[j] = up + left;
+    //             }
+    //         }
+    //         prev =curr;
+    //     }
+    //     return prev[n-1] ;
+    // }
+
+
+    // tabulation
     int uniquePaths(int m, int n) {
-        vector<int> prev(n,0);
-        // base case 
-        
+        vector<vector<int>> dp (m, vector<int> (n,0));
 
         for( int i = 0; i < m; i++){
-            vector<int> curr (n,0);
             for(int j = 0; j < n; j++){
+
                 if(i == 0 && j == 0){
-                    curr[j] = 1;
+                    dp[i][j] = 1;
                 }
+                
                 else{
-                    int up = (i>0) ? prev[j] :0 ;
-                    int left = (j>0) ? curr[j-1] : 0 ;
-                    curr[j] = up + left;
+                    int up = (i>0) ? dp[i-1][j] :0 ;
+                    int left = (j>0) ? dp[i][j-1] : 0 ;
+                    dp[i][j] = up + left;
                 }
             }
-            prev =curr;
         }
-        return prev[n-1] ;
+        return dp[m-1][n-1] ;
     }
-
 
 
 
