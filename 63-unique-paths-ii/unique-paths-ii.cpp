6b8edf458b47b3b1318ba01f414d3df227212123
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-14 17:32:24 +0530

    Time: 3 ms (53.55%) | Memory: 10.3 MB (8.39%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int paths(int i, int j, vector<vector<int>> &dp,vector<vector<int>>& grid){
+        //check if its a valid cell and has a obstacle 
+        if(i>=0 && j>=0 && grid[i][j] == 1){
+            return 0;
+        }
         //base cases 
-        if(i == 0 && j == 0 && grid[i][j] != 1){
+        if(i == 0 && j == 0){
             return 1;
         }
-        
         if(i<0 || j<0){   // out of bounds 
             return 0;
         }
-        if(grid[i][j] == 1){
-            return 0;
-        }
+        
         // check is ans is already present 
         if(dp[i][j] != -1){
             return dp[i][j];
@@ -30,4 +31,24 @@ public:
         vector<vector<int>> dp (m, vector<int> (n,-1));
         return paths(m-1, n-1, dp, obstacleGrid);
     }
+    // int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    //     vector<vector<int>> dp (m, vector<int> (n,0));
+
+    //     for( int i = 0; i < m; i++){
+    //         for(int j = 0; j < n; j++){
+
+    //             if(i == 0 && j == 0){
+    //                 dp[i][j] = 1;
+    //             }
+                
+    //             else{
+    //                 int up = (i-1>=0) ? dp[i-1][j] :0 ;
+    //                 int left = (j-1>=0) ? dp[i][j-1] : 0 ;
+    //                 dp[i][j] = up + left;
+    //             }
+    //         }
+    //     }
+    //     return dp[m-1][n-1] ;
+    // }
+    
 };
