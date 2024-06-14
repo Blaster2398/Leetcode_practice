0ee102a399f2852734398fc590540ecca9f8b5f1
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-14 18:59:11 +0530

    Time: 3 ms (95.01%) | Memory: 12.6 MB (33.35%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -19,11 +19,34 @@ public:
         return dp[i][j] = min(up,left);
 
     }
-    int minPathSum(vector<vector<int>>& grid) {
+    int minPathSum_recursive(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp (m, vector<int> (n,-1));
         return paths(m-1, n-1, dp, grid);
+    }
+
+     // tabulation
+    int minPathSum(vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp (m, vector<int> (n,0));
+
+        for( int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[0][0];
+                }
+                
+                else{
+                    int up = (i-1>=0) ? grid[i][j]+dp[i-1][j] : 1e9 ;
+                    int left = (j-1>=0) ? grid[i][j]+dp[i][j-1] : 1e9 ;
+                    dp[i][j] = min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1] ;
     }
 
 };
