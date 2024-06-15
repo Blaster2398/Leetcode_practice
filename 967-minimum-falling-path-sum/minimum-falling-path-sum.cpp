da54e3eee301bd68b3f6f8c0085751bc4bcd68c5
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-15 19:50:14 +0530

    Time: 21 ms (5.15%) | Memory: 13.5 MB (69.82%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,61 +1,91 @@
 #define MAX 1e9
 class Solution {
-// private:
-//     int fall(vector<vector<int>>& matrix, int n, int i, int j, vector<vector<int>>& dp){
-//         //out of bound and base cases 
-//         if(j < 0 || j >= n){
-//             return MAX;
-//         }
-//         if(i == 0){
-//             return matrix[i][j];
-//         }
-//         if(dp[i][j] != -1){
-//             return dp[i][j];
-//         }
-//         int lu = matrix[i][j] + fall(matrix, n, i-1, j-1, dp);
-//         int u = matrix[i][j] + fall(matrix, n, i-1, j, dp);
-//         int ru = matrix[i][j] + fall(matrix, n, i-1, j+1, dp);
+private:
+    int fall(vector<vector<int>>& matrix, int n, int i, int j, vector<vector<int>>& dp){
+        //out of bound and base cases 
+        if(j < 0 || j >= n){
+            return MAX;
+        }
+        if(i == 0){
+            return matrix[i][j];
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int lu = matrix[i][j] + fall(matrix, n, i-1, j-1, dp);
+        int u = matrix[i][j] + fall(matrix, n, i-1, j, dp);
+        int ru = matrix[i][j] + fall(matrix, n, i-1, j+1, dp);
 
-//         return dp[i][j] = min(lu, min(u, ru));
-//     }
-// public:
-//     int minFallingPathSum(vector<vector<int>>& matrix) {
-//         int n = matrix.size();
-//         if (n == 0) return 0; // Edge case: empty matrix
+        return dp[i][j] = min(lu, min(u, ru));
+    }
+public:
+    int minFallingPathSum_recursive(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        if (n == 0) return 0; // Edge case: empty matrix
 
-//         vector<vector<int>> dp(n, vector<int>(n,-1));
-//         int mini = MAX;
-//         for(int i =0 ; i< n; i++){
-//             int sum = fall(matrix, n ,n-1, i, dp);
-//             mini = min(mini,sum);
-//         }
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        int mini = MAX;
+        for(int i =0 ; i< n; i++){
+            int sum = fall(matrix, n ,n-1, i, dp);
+            mini = min(mini,sum);
+        }
 
-//         return mini;
-//     }
+        return mini;
+    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
+        // vector<vector<int>> dp(n, vector<int>(n,0));
+        vector<int> prev(n);
         
         // base case when it reaches i= 0 fill the dpp
         for(int j = 0; j <n; j++){
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
 
         for(int i = 1; i < n; i++){
+            vector<int> curr(n);
             for(int j = 0; j < n; j++){
-                    int lu = matrix[i][j] + ((j>0) ? dp[i-1][j-1] : MAX);
-                    int u = matrix[i][j] + dp[i-1][j];
-                    int ru = matrix[i][j] + ((j<n-1) ? dp[i-1][j+1] : MAX);
+                    int lu = matrix[i][j] + ((j>0) ? prev[j-1] : MAX);
+                    int u = matrix[i][j] + prev[j];
+                    int ru = matrix[i][j] + ((j<n-1) ? prev[j+1] : MAX);
                 
-                dp[i][j] = min(lu, min(u, ru));
+                curr[j] = min(lu, min(u, ru));
             }
+            prev = curr;
         }
 
         int mini = MAX;
         for(int j = 0; j < n; j++){
-            mini = min(dp[n-1][j], mini);
+            mini = min(prev[j], mini);
         }
         return mini;
     }
+
+
+    // int minFallingPathSum(vector<vector<int>>& matrix) {
+    //     int n = matrix.size();
+    //     vector<vector<int>> dp(n, vector<int>(n,0));
+        
+    //     // base case when it reaches i= 0 fill the dpp
+    //     for(int j = 0; j <n; j++){
+    //         dp[0][j] = matrix[0][j];
+    //     }
+
+    //     for(int i = 1; i < n; i++){
+    //         for(int j = 0; j < n; j++){
+    //                 int lu = matrix[i][j] + ((j>0) ? dp[i-1][j-1] : MAX);
+    //                 int u = matrix[i][j] + dp[i-1][j];
+    //                 int ru = matrix[i][j] + ((j<n-1) ? dp[i-1][j+1] : MAX);
+                
+    //             dp[i][j] = min(lu, min(u, ru));
+    //         }
+    //     }
+
+    //     int mini = MAX;
+    //     for(int j = 0; j < n; j++){
+    //         mini = min(dp[n-1][j], mini);
+    //     }
+    //     return mini;
+    // }
 };
