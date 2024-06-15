24fd4b3d2de3c97c409b25b8f78f3543894ff26a
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-15 17:50:54 +0530

    Time: 8 ms (27.01%) | Memory: 11.5 MB (10.32%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -15,9 +15,31 @@ public:
 
         return dp[i][j] = min(d, dg);
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
+    int minimumTotal_recusive(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n,-1));
         return Triangle(triangle, n, 0, 0, dp);
+    }
+
+
+    // tabulation starts from the opposite of memoization
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n,0));
+
+        // base cases 
+        for(int j = n-1; j >= 0; j--){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                int d =  triangle[i][j] + dp[i+1][j];
+                int dg = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(d, dg);
+            }
+        }
+        return dp[0][0];
     }
 };
