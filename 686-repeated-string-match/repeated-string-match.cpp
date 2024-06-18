62cace5788fffab16d858261f14e85158f1a0cdd
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-18 16:55:16 +0530

    Time: 56 ms (18.97%) | Memory: 482.7 MB (5.04%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
-    bool strStr(string haystack, string needle){
-        int n = needle.size();
+    bool strStr(string haystack, string needle) {
         int h = haystack.size();
-
-        // if(n ==0) return true;
-        vector<int> lps(n, 0);
-        int prev = 0;
-        int curr = 1;
-
-        while(curr < n){
-            if(needle[curr] == needle[prev]){
-                lps[curr] = prev+1;
-                prev++;
-                curr++;
-            }
-            else if(prev == 0){
-                lps[curr] = 0;
-                curr++;
-            }
-            else{
-                prev = lps[prev -1];
-            }   
-        }
-        // pattern matching 
-        int i = 0;
-        int j = 0;
-        while(i < h){
-            if(haystack[i] == needle[j]){
-                i++;
-                j++;
-            }
-            else if(j == 0){
-                i++;
-            }
-            else{
-                j = lps[j-1];
-            }
-            if(j == n){
+        int n = needle.size();
+        for(int i = 0; i <= h - n +1; i++){
+            if(haystack.substr(i,n) == needle){
                 return true;
             }
         }
         return false;
     }
+    // bool strStr(string haystack, string needle){
+    //     int n = needle.size();
+    //     int h = haystack.size();
+
+    //     // if(n ==0) return true;
+    //     vector<int> lps(n, 0);
+    //     int prev = 0;
+    //     int curr = 1;
+
+    //     while(curr < n){
+    //         if(needle[curr] == needle[prev]){
+    //             lps[curr] = prev+1;
+    //             prev++;
+    //             curr++;
+    //         }
+    //         else if(prev == 0){
+    //             lps[curr] = 0;
+    //             curr++;
+    //         }
+    //         else{
+    //             prev = lps[prev -1];
+    //         }   
+    //     }
+    //     // pattern matching 
+    //     int i = 0;
+    //     int j = 0;
+    //     while(i < h){
+    //         if(haystack[i] == needle[j]){
+    //             i++;
+    //             j++;
+    //         }
+    //         else if(j == 0){
+    //             i++;
+    //         }
+    //         else{
+    //             j = lps[j-1];
+    //         }
+    //         if(j == n){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
     int repeatedStringMatch(string a, string b) {
         if(b.length() == 0) return 0;
 
