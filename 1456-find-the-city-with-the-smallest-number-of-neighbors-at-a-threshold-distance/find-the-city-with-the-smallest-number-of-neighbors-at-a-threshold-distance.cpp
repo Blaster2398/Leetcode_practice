05939f92035daf896621af0a274ee64729ceea2c
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-22 19:21:56 +0530

    Time: 25 ms (67.89%) | Memory: 15.3 MB (81.92%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,64 +1,54 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        // Initialize adjacency matrix with "infinity"
         vector<vector<int>> mat(n, vector<int>(n, 1e9));
-        for(auto e : edges){
+        
+        // Set edge weights
+        for (const auto& e : edges) {
             int i = e[0];
             int j = e[1];
             int wt = e[2];
             mat[i][j] = wt;
             mat[j][i] = wt;
         }
-        for(int i = 0; i <n; i++){
+
+        // Set the diagonal to zero (distance from a node to itself)
+        for (int i = 0; i < n; i++) {
             mat[i][i] = 0;
         }
 
-        for(int i = 0; i <n; i++){
-            for(int j =0;j <n; j++){
-                cout << mat[i][j] << " " ;
-            }
-
-            cout << endl;
-        }
-        cout << endl;
-        cout << endl;
-        
-        for(int via = 0; via <n; via++){
-            for(int i = 0; i <n; i++){
-                for(int j =0;j <n; j++){
-                    mat[i][j] = min(mat[i][j], mat[i][via] + mat[via][j]);
+        // Floyd-Warshall algorithm to find all-pairs shortest paths
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i][via] < 1e9 && mat[via][j] < 1e9) { // Avoid overflow
+                        mat[i][j] = min(mat[i][j], mat[i][via] + mat[via][j]);
+                    }
                 }
             }
         }
 
-        for(int i = 0; i <n; i++){
-            for(int j =0;j <n; j++){
-                cout << mat[i][j] << " " ;
-            }
-
-            cout << endl;
-        }
-
-        vector<int> res(n, 0);
-
-        for(int i = 0; i <n; i++){
-            for(int j =0;j <n; j++){
-                if(mat[i][j] <= distanceThreshold && mat[i][j] != 0){
-                    res[i]++;
+        // Count reachable cities within the distance threshold
+        vector<int> reachableCities(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && mat[i][j] <= distanceThreshold) {
+                    reachableCities[i]++;
                 }
             }
         }
 
-        int mini = 1e9;
-        int ind = 0;
-        for(int i = 0; i < n; i++){
-            cout << "res[" << i << "] " << res[i] <<endl; 
-            if(res[i] <= mini){
-                mini = res[i];
-                ind = i;
+        // Find the city with the smallest number of reachable cities within the threshold
+        int minReachable = INT_MAX;
+        int city = -1;
+        for (int i = 0; i < n; i++) {
+            if (reachableCities[i] <= minReachable) {
+                minReachable = reachableCities[i];
+                city = i;
             }
         }
-        return ind;
 
+        return city;
     }
 };
