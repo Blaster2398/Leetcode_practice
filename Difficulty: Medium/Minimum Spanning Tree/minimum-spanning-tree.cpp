749fb0a8f922d19204e848162c81c52978bd3b8d
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-23 15:16:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -4,44 +4,114 @@ using namespace std;
 
 // } Driver Code Ends
 
+class DSU
+{
+    
+    public:
+    vector<int> rank;
+    vector<int> parent;
+    
+    DSU (int V){
+        parent.resize(V);
+        rank.resize(V, 0);  
+        for(int i = 0; i < V; i++){
+            parent[i] = i;
+        }
+    }
+    
+    int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+        else{
+            return parent[x] = find(parent[x]);
+        }
+    }
+    
+    void union_rank(int x, int y){
+        int a = find(x);
+        int b = find(y);
+        
+        if(a == b) return ;
+        
+        if(rank[a] < rank[b]) parent[a] = b;
+        else if(rank[a] > rank[b]) parent[b] = a;
+        else{
+            parent[b] = a;
+            rank[a++];
+        }
+    }
+};
+
 class Solution
 {
     // typedef pair<int,int> pair<int,int>;
-	public:
 	
+	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        int res = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>>> pq;
-        vector<bool> visited(V, false);
-        
-        pq.push({0,0});
-        
-        while(!pq.empty()){
-            auto ft = pq.top();
-            int u = ft.second;
-            int wt = ft.first;
-            pq.pop();
-            
-            if(visited[u] == true){
-                continue;
-            }
-            
-            visited[u] = true;
-            res = res + wt;
-            
-            for(auto v : adj[u]){
-                int nextNode = v[0];
-                int dis = v[1];
-                if(visited[nextNode] == false){
-                    pq.push({dis,nextNode});
-                }
+        int cost = 0;
+        vector<pair<int, pair<int, int>>> edges;
+        for(int u=0; u<V; u++){
+            for(int j=0; j<adj[u].size(); j++){
+                
+                int v = adj[u][j][0];
+                int w = adj[u][j][1];
+                
+                edges.push_back({w, {u, v}});
+                
             }
         }
         
-        return res;
+        sort(edges.begin(), edges.end());
+        
+        DSU ds(V);
+        for(int i = 0; i < edges.size(); i++){
+            int u = edges[i].second.first;
+            int v = edges[i].second.second;
+            int w = edges[i].first;
+            
+            if(ds.find(u) != ds.find(v)){
+                cost+= w;
+                ds.union_rank(u,v);
+            }
+        }
+        
+        return cost;
     }
+    // int spanningTree_Prim(int V, vector<vector<int>> adj[])
+    // {
+    //     int res = 0;
+    //     priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>>> pq;
+    //     vector<bool> visited(V, false);
+        
+    //     pq.push({0,0});
+        
+    //     while(!pq.empty()){
+    //         auto ft = pq.top();
+    //         int u = ft.second;
+    //         int wt = ft.first;
+    //         pq.pop();
+            
+    //         if(visited[u] == true){
+    //             continue;
+    //         }
+            
+    //         visited[u] = true;
+    //         res = res + wt;
+            
+    //         for(auto v : adj[u]){
+    //             int nextNode = v[0];
+    //             int dis = v[1];
+    //             if(visited[nextNode] == false){
+    //                 pq.push({dis,nextNode});
+    //             }
+    //         }
+    //     }
+        
+    //     return res;
+    // }
 };
 
 //{ Driver Code Starts.
