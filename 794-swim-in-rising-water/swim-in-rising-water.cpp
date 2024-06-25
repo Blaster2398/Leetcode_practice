362ab3f9f64fdff27b12b2783e4885e821261529
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-25 09:23:29 +0530

    Time: 6 ms (96.99%) | Memory: 12.3 MB (88.21%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -5,46 +5,44 @@ public:
         
         // Source and destination
         pair<int, int> src = {0, 0};
-        pair<int, int> des = {n-1, n-1};
+        pair<int, int> dest = {n-1, n-1};
 
-        // Initialize the queue for BFS  {dist , {i,j}}
+        // Initialize the queue for BFS {time, {x, y}}
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
 
-        vector<vector<int>> dist(n, vector<int>(n, 1e9));
+        vector<vector<int>> time(n, vector<int>(n, 1e9));
         // Initial push of the source node
         pq.push({grid[src.first][src.second], src});
-        dist[src.first][src.second] = grid[src.first][src.second];
+        time[src.first][src.second] = grid[src.first][src.second];
 
         // Directions for 4 possible movements
         vector<int> dx = {0, 1, 0, -1};
-        vector<int> dy = {1, 0, -1 , 0};
+        vector<int> dy = {1, 0, -1, 0};
 
         while (!pq.empty()) {
-            auto it = pq.top();
+            auto curr = pq.top();
             pq.pop();
-            int diff = it.first;
-            int i1 = it.second.first;
-            int j1 = it.second.second;
-            cout << "i1 " << i1 << " j1 " << j1 << " diff " <<diff <<endl;
+            int currTime = curr.first;
+            int x = curr.second.first;
+            int y = curr.second.second;
 
-            if (i1 == n - 1 && j1 == n - 1)
-                return diff;
+            if (x == n - 1 && y == n - 1)
+                return currTime;
 
             for (int i = 0; i < 4; i++) {
-                int nx = i1 + dx[i];
-                int ny = j1 + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n ) {
-                    int newEffort = max(grid[nx][ny], diff);
-                    if(newEffort < dist[nx][ny]){
-                        dist[nx][ny] = newEffort;
-                        pq.push({dist[nx][ny],{nx,ny}});
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    int newTime = max(grid[nx][ny], currTime);
+                    if (newTime < time[nx][ny]) {
+                        time[nx][ny] = newTime;
+                        pq.push({newTime, {nx, ny}});
                     }
                 }
             }
         }
 
-        return 0;
-        
+        return time[n-1][n-1];
     }
 };
