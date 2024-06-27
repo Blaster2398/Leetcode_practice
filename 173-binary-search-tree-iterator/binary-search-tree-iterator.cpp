825ada31cca4a4f723e5552b2ee2860adb7c9fe1
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-27 16:08:30 +0530

    Time: 12 ms (96.48%) | Memory: 27.7 MB (91.14%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -11,37 +11,66 @@
  */
 class BSTIterator {
 public:
-    int i = 0;
-    vector<int> temp;
-
-    void INO(TreeNode* root){
+    stack<TreeNode*> st;
+    void gotoleft(TreeNode* root){
         if(root == NULL) return;
-
-        INO(root->left);
-        temp.push_back(root->val);
-        INO(root->right);
+        while(root){
+            st.push(root);
+            root = root->left;
+        }
     }
-
     BSTIterator(TreeNode* root) { 
-        temp.push_back(INT_MIN);
-        INO(root);
+        gotoleft(root);
     }
     
     int next() {
-        i++;
-        int siz = temp.size();
-        if(i <= siz){
-            return temp[i]; 
+        TreeNode* ft = st.top();
+        st.pop();
+        if(ft->right){
+            gotoleft(ft->right);
         }
-        return -1;
+        return ft->val;
     }
     
     bool hasNext() {
-        cout << i <<" ";
-        cout << temp.size() << endl;
-        return (i < temp.size()-1);
+        return (st.size()>0);
     }
 };
+
+
+// class BSTIterator {
+// public:
+//     int i = 0;
+//     vector<int> temp;
+
+//     void INO(TreeNode* root){
+//         if(root == NULL) return;
+
+//         INO(root->left);
+//         temp.push_back(root->val);
+//         INO(root->right);
+//     }
+
+//     BSTIterator(TreeNode* root) { 
+//         temp.push_back(INT_MIN);
+//         INO(root);
+//     }
+    
+//     int next() {
+//         i++;
+//         int siz = temp.size();
+//         if(i <= siz){
+//             return temp[i]; 
+//         }
+//         return -1;
+//     }
+    
+//     bool hasNext() {
+//         cout << i <<" ";
+//         cout << temp.size() << endl;
+//         return (i < temp.size()-1);
+//     }
+// };
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
