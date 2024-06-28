424eabe5ae2f10e633dc12167928fcaecaf35857
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-28 10:55:29 +0530

    Time: 14 ms (47.28%) | Memory: 17.4 MB (81.82%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -18,8 +18,19 @@ public:
 
 class Solution {
 public:
-    
     Node* connect(Node* root) {
+        if(!root || (!root->left && !root->right)) return root;
+
+        root->left->next = root->right;
+        root->right->next = root->next ? root->next->left : NULL;
+
+        connect(root->left);
+        connect(root->right);
+
+        return root;
+    }
+    
+    Node* connect2(Node* root) {
         if(root == NULL)return root;
 
         queue<Node*> q;
