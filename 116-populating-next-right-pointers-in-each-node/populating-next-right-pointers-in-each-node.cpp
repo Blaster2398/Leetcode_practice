42d08b188929ec38e8ad4b2a94fd357e88790426
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-28 10:53:47 +0530

    Time: 20 ms (8.34%) | Memory: 17.7 MB (49.14%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    
+    Node* connect(Node* root) {
+        if(root == NULL)return root;
+
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+
+                Node* node = q.front();
+                q.pop();
+
+                // push the left and right if they are pressent
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+
+                Node* to_connect = q.front();
+
+                // for the last node we dont have anything to connect
+                if(i == size-1){
+                    node->next = NULL;
+                }
+                else {
+                    node->next = to_connect;
+                }
+            }
+        }
+        return root;
+    }
+};
