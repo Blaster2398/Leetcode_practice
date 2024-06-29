594d9ea5fdb9f12396ce7c8a05d9e4f0acc17ce1
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-29 21:10:29 +0530

    Time: 87 ms (95.26%) | Memory: 77.6 MB (97.69%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -22,10 +22,23 @@ public:
 
         return dp[ind][buy][maxd] = profit;
     }
-    int maxProfit(vector<int>& prices) {
+    int maxProfit_memo(vector<int>& prices) {
         // vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
         vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
         // cout << util(prices, 0, 1, 2) << endl;
         return util(prices, 0, 1,dp ,2);
     }
+
+    // there is O(n) solution also 
+    int maxProfit(vector<int>& prices) {
+        int f1 = -prices[0], f2 = 0, f3 = -prices[0], f4 = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            f1 = max(f1, -prices[i]);
+            f2 = max(f2, f1 + prices[i]);
+            f3 = max(f3, f2 - prices[i]);
+            f4 = max(f4, f3 + prices[i]);
+        }
+        return f4;
+    }
+    
 };
