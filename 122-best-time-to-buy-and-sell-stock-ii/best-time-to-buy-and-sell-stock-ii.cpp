a9fb81e6f53317366d3e429affa3dc59de5504f0
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-29 18:21:38 +0530

    Time: 5 ms (59.64%) | Memory: 15.5 MB (47.67%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -23,35 +23,52 @@ public:
         return util(prices, 0, 1, dp);
     }
 
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2,0));
+        vector<int> ahead(2,0), curr(2,0);
 
         //base case
-        dp[n][0] = dp[n][1] = 0;
+        ahead[0] = ahead[1] = 0;
 
         // as it is top doun go from n-1 to 0
         for(int ind = n-1; ind >= 0; ind--){
             for(int buy = 0; buy <=1; buy++){
                 int profit = 0;
                 if(buy){
-                    profit = max(-prices[ind]+ dp[ind+1][0], 0 + dp[ind+1][1]);
+                    profit = max(-prices[ind]+ ahead[0], 0 + ahead[1]);
                 } else {
-                    profit = max(prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0]);
+                    profit = max(prices[ind] + ahead[1], 0 + ahead[0]);
                 }
-                dp[ind][buy] = profit;
+                curr[buy] = profit;
             }
+            ahead = curr;
         }
-        return dp[0][1];
+        return ahead[1];
     }
 
 
+    // int maxProfit(vector<int>& prices) {
+    //     int n = prices.size();
+    //     vector<vector<int>> dp(n+1, vector<int>(2,0));
 
+    //     //base case
+    //     dp[n][0] = dp[n][1] = 0;
 
-
-
-
-
+    //     // as it is top doun go from n-1 to 0
+    //     for(int ind = n-1; ind >= 0; ind--){
+    //         for(int buy = 0; buy <=1; buy++){
+    //             int profit = 0;
+    //             if(buy){
+    //                 profit = max(-prices[ind]+ dp[ind+1][0], 0 + dp[ind+1][1]);
+    //             } else {
+    //                 profit = max(prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0]);
+    //             }
+    //             dp[ind][buy] = profit;
+    //         }
+    //     }
+    //     return dp[0][1];
+    // }
 
 
     // int maxProfit(vector<int>& prices) {
