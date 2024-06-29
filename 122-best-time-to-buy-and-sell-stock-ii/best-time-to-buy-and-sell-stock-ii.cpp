b58872472bc5398bba5d1cd15d14246dad2edb28
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-29 17:55:56 +0530

    Time: 17 ms (5.12%) | Memory: 20.3 MB (9.28%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int maxProfit = 0;
-        int buyPrice = prices[0];
-        for(int i = 0; i< prices.size(); i++){
-            if(buyPrice < prices[i]){
-                int profit = prices[i] - buyPrice;
-                maxProfit += profit;
-                buyPrice = prices[i];
-            }
-            else{
-                buyPrice = prices[i];
-            }
+    int util(vector<int>& prices, int ind, int buy,vector<vector<int>>& dp){
+        // base cases 
+        if(ind == prices.size()){
+            return 0;
         }
-        return maxProfit;
+        if(dp[ind][buy] != -1){
+            return dp[ind][buy];
+        }
+        // steps
+        int profit = 0;
+        if(buy){
+            profit = max(-prices[ind]+ util(prices, ind+1, 0, dp), 0 + util(prices, ind+1, 1, dp));
+        } else {
+            profit = max(prices[ind] + util(prices, ind+1, 1, dp), 0 + util(prices, ind+1, 0, dp));
+        }
+
+        return dp[ind][buy] = profit;
     }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
+        return util(prices, 0, 1, dp);
+    }
+    // int maxProfit(vector<int>& prices) {
+    //     int maxProfit = 0;
+    //     int buyPrice = prices[0];
+    //     for(int i = 0; i< prices.size(); i++){
+    //         if(buyPrice < prices[i]){
+    //             int profit = prices[i] - buyPrice;
+    //             maxProfit += profit;
+    //             buyPrice = prices[i];
+    //         }
+    //         else{
+    //             buyPrice = prices[i];
+    //         }
+    //     }
+    //     return maxProfit;
+    // }
 };
