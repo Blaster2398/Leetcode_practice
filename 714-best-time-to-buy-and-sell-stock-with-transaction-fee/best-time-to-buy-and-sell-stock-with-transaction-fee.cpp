a9a90cfb1b54b4250b433777d9050c763b0e5b70
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-30 00:58:21 +0530

    Time: 143 ms (54.94%) | Memory: 93.3 MB (46.26%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -19,8 +19,35 @@ public:
 
         return dp[ind][buy] = profit;
     }
-    int maxProfit(vector<int>& prices, int fee) {
+    int maxProfit_memo(vector<int>& prices, int fee) {
         vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
         return util(prices, 0, 1, dp, fee);
+    }
+
+
+
+
+
+    // TAbulatin
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+
+        //base case
+        dp[n][0] = dp[n][1] = 0;
+
+        // as it is top doun go from n-1 to 0
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int buy = 0; buy <=1; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-prices[ind]+ dp[ind+1][0], 0 + dp[ind+1][1]);
+                } else {
+                    profit = max(prices[ind] + dp[ind+1][1] -fee, 0 + dp[ind+1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
