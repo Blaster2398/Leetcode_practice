099ee382a0fd6bd6d184d31278e0ca90b19e9eb0
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-06-29 18:11:58 +0530

    Time: 8 ms (34.84%) | Memory: 17.8 MB (19.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,10 +18,42 @@ public:
 
         return dp[ind][buy] = profit;
     }
-    int maxProfit(vector<int>& prices) {
+    int maxProfit_recursive(vector<int>& prices) {
         vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
         return util(prices, 0, 1, dp);
     }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+
+        //base case
+        dp[n][0] = dp[n][1] = 0;
+
+        // as it is top doun go from n-1 to 0
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int buy = 0; buy <=1; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-prices[ind]+ dp[ind+1][0], 0 + dp[ind+1][1]);
+                } else {
+                    profit = max(prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+
+
+
+
+
+
+
+
+
+
     // int maxProfit(vector<int>& prices) {
     //     int maxProfit = 0;
     //     int buyPrice = prices[0];
