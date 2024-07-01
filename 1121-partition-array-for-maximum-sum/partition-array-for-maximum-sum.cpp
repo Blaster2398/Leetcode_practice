97ba995445fabe3e88178e926916126c2b139bab
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-01 19:34:22 +0530

    Time: 8 ms (66.59%) | Memory: 10.8 MB (89.23%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -24,34 +24,49 @@ public:
     // }
 
 
+    // //memoizatopn
+    // int f(vector<int>& arr, int k, int i, vector<int> & dp){
+    //     int n = arr.size();
+    //     // base case 
+    //     if(i >= arr.size()) return 0;
 
-    int f(vector<int>& arr, int k, int i, vector<int> & dp){
-        int n = arr.size();
-        // base case 
-        if(i >= arr.size()) return 0;
-
-        if(dp[i] != -1 ) return dp[i];
-        // do some stuff
-        int maxi = 0;
-        int len = 0;
-        int maxele = arr[i];
-        for(int j = i; j < min(i + k, n); j++){
-            len++;
-            maxele = max(maxele, arr[j]);
-            int sum = (len * maxele) + f(arr, k, j+1, dp);
-            maxi = max(maxi, sum);
-        }
-        return dp[i] = maxi;
-    }
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp( arr.size() , -1);
-        return f(arr, k, 0, dp);
-    }
-
-
-
+    //     if(dp[i] != -1 ) return dp[i];
+    //     // do some stuff
+    //     int maxi = 0;
+    //     int len = 0;
+    //     int maxele = arr[i];
+    //     for(int j = i; j < min(i + k, n); j++){
+    //         len++;
+    //         maxele = max(maxele, arr[j]);
+    //         int sum = (len * maxele) + f(arr, k, j+1, dp);
+    //         maxi = max(maxi, sum);
+    //     }
+    //     return dp[i] = maxi;
+    // }
     // int maxSumAfterPartitioning(vector<int>& arr, int k) {
     //     vector<int> dp( arr.size() , -1);
     //     return f(arr, k, 0, dp);
     // }
+
+
+
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp( arr.size()+ 1 , 0);
+        
+        for(int i = n-1; i >=0 ; i--){
+            cout << "done " << i << endl;
+            int maxi = 0;
+            int len = 0;
+            int maxele = arr[i];
+            for(int j = i; j < min(i + k, n); j++){
+                len++;
+                maxele = max(maxele, arr[j]);
+                int sum = (len * maxele) + dp[j+1];
+                maxi = max(maxi, sum);
+            }
+            dp[i] = maxi;
+        }
+        return dp[0];
+    }
 };
