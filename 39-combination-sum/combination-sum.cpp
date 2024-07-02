66c8067e34ea22a0d697ef9d997a541f4ea80db8
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-02 10:01:03 +0530

    Time: 4 ms (73.09%) | Memory: 12.5 MB (54.61%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void f(vector<int>& nums, int t, int ind, vector<vector<int>>& st, vector<int> temp, int currSum){
+    void f(vector<int>& nums, int t, int ind, vector<vector<int>>& st, vector<int>& temp, int currSum){
         // base cases 
         if(currSum == t){
             st.push_back(temp);
@@ -15,14 +15,14 @@ public:
         temp.push_back(nums[ind]);
         f(nums, t, ind, st, temp, currSum + nums[ind]);
         temp.pop_back();
-        
         f(nums, t, ind+1, st, temp, currSum);
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> st;
+        vector<int> temp;
         sort(candidates.begin(), candidates.end());
-        f(candidates, target, 0, st, {}, 0);
+        f(candidates, target, 0, st, temp, 0);
 
         return st;
         
