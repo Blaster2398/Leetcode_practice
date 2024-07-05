8953c1c6abfc42ed7f577c89863fbe279f9af803
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-05 19:38:03 +0530

    Time: 444 ms (5.01%) | Memory: 94.1 MB (14.52%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,59 +1,69 @@
 class Solution {
 public:
+    // int largestRectangleArea(vector<int>& heights) {
+    //     // you have to 1st male 2 vectors smalles left element and smallest right element these will store the indices 
+    //     // for finding the aread you can take the blocks which are ahead of the leeft smallest and before thr right smallest
+    //     int n = heights.size();
+        
+    // }
+
+
+
+
     int largestRectangleArea(vector<int>& heights) {
         // you have to 1st male 2 vectors smalles left element and smallest right element these will store the indices 
         // for finding the aread you can take the blocks which are ahead of the leeft smallest and before thr right smallest
         int n = heights.size();
         vector<int> small;
         vector<int> large;
-        stack<pair<int, int>> temp;
-        stack<pair<int, int>> temp2;
+        stack<int> temp;
+        stack<int> temp2;
 
         for(int i =0 ;i < n; i++){
             if(temp.empty()){
                 small.push_back(-1);
             }
             else{
-                if(heights[i] > temp.top().first){
-                    small.push_back(temp.top().second);
+                if(heights[i] > heights[temp.top()]){
+                    small.push_back(temp.top());
                 }
                 else{
-                    while(!temp.empty() && heights[i] <= temp.top().first){
+                    while(!temp.empty() && heights[i] <= heights[temp.top()]){
                         temp.pop();
                     }
                     if(temp.empty()){
                         small.push_back(-1);
                     }
                     else{
-                        small.push_back(temp.top().second);
+                        small.push_back(temp.top());
 
                     }
                 }
             }
-            temp.push({heights[i], i});
+            temp.push(i);
         }
         for(int i = n-1;i >= 0; i--){
             if(temp2.empty()){
                 large.push_back(n);
             }
             else{
-                if(heights[i] > temp2.top().first){
-                    large.push_back(temp2.top().second);
+                if(heights[i] > heights[temp2.top()]){
+                    large.push_back(temp2.top());
                 }
                 else{
-                    while(!temp2.empty() && heights[i] <= temp2.top().first){
+                    while(!temp2.empty() && heights[i] <= heights[temp2.top()]){
                         temp2.pop();
                     }
                     if(temp2.empty()){
                         large.push_back(n);
                     }
                     else{
-                        large.push_back(temp2.top().second);
+                        large.push_back(temp2.top());
 
                     }
                 }
             }
-            temp2.push({heights[i], i});
+            temp2.push(i);
         }
 
         for(auto i : small){
