b62ce516af960345067587b628015dc57fb9c83c
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-06 11:01:35 +0530

    Time: 13 ms (84.96%) | Memory: 21.1 MB (16.15%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,38 +1,88 @@
 class MinStack {
 public:
-    stack<int> st;
-    // priority_queue<int, vector<int>, greater<int>> pq;
-    map<int,int> mp;
-    MinStack() {
-        
-    }
-    
-    void push(int val) {
-        st.push(val);
-        // pq.push(val);
-        mp[val]++;
-    }
-    
-    void pop() {
-        int t = st.top();
-        st.pop();
-        if(mp[t] > 0){
-            mp[t]--;
-        }
-        if(mp[t] == 0){
-            mp.erase(t);
-        }
-    }
-    
-    int top() {
-        return st.top();
-    }
-    
-    int getMin() {
-        auto it = mp.begin();
+    stack<long long> st;
+    long long mini = INT_MAX;
 
-        return it->first;
+    MinStack() {
+        while(!st.empty()) st.pop();
     }
+
+    void push(int value) {
+        long long val = value;
+        if(st.empty()) {
+            mini = val;
+            st.push(val);
+        } else {
+            if(val < mini) {
+                st.push(2*val*1LL - mini);
+                mini = val;
+            } else {
+                st.push(val);
+            }
+        }
+    }
+
+    void pop() {
+        if(st.empty()) return;
+
+        long long t = st.top();
+        st.pop();
+        if(t < mini) {
+            mini = 2*mini - t;
+        }
+    }
+
+    int top() {
+        if(st.empty()) return -1;
+        long long t = st.top();
+        if(t < mini) {
+            return mini;
+        } else {
+            return t;
+        }
+    }
+
+    int getMin() {
+        return mini;
+    }
+
+
+
+
+
+//     stack<int> st;
+//     // priority_queue<int, vector<int>, greater<int>> pq;
+//     map<int,int> mp;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+//         // pq.push(val);
+//         mp[val]++;
+//     }
+    
+//     void pop() {
+//         int t = st.top();
+//         st.pop();
+//         if(mp[t] > 0){
+//             mp[t]--;
+//         }
+//         if(mp[t] == 0){
+//             mp.erase(t);
+//         }
+//     }
+    
+//     int top() {
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         auto it = mp.begin();
+
+//         return it->first;
+//     }
 };
 
 /**
