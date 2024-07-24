a635a4cacd794b212267d6de48e2f40d0e239f88
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-24 13:23:12 +0530

    Time: 11 ms (54.28%) | Memory: 18.2 MB (25.43%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -10,8 +10,8 @@ public:
             return dp[ind];
         }
 
-        for(int i = ind + 1; i <= s.size(); i++){
-            if(dict.count(s.substr(ind, i - ind )) && canBreak(s, dict, i, dp)){
+        for(int i = ind ; i < s.size(); i++){
+            if(dict.count(s.substr(ind, i - ind + 1)) && canBreak(s, dict, i + 1, dp)){
                 return dp[ind] = true;
             }
         }
@@ -24,4 +24,30 @@ public:
 
         return canBreak(s, dict,0, dp);
     }
+
+
+
+    // bool canBreak(string s, unordered_set<string>& dict, int ind, vector<int> & dp){
+    //     if(ind == s.size()){
+    //         return true;
+    //     }
+
+    //     if(dp[ind] != -1){
+    //         return dp[ind];
+    //     }
+
+    //     for(int i = ind + 1; i <= s.size(); i++){
+    //         if(dict.count(s.substr(ind, i - ind )) && canBreak(s, dict, i, dp)){
+    //             return dp[ind] = true;
+    //         }
+    //     }
+
+    //     return dp[ind] = false;
+    // }
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     unordered_set<string> dict(wordDict.begin(), wordDict.end());
+    //     vector<int> dp(s.size() +1, -1);
+
+    //     return canBreak(s, dict,0, dp);
+    // }
 };
