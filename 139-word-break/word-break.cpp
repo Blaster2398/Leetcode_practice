15d2ec040f62a795f6a1e27423898ec40b45d8ca
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-24 13:20:32 +0530

    Time: 16 ms (38.92%) | Memory: 18.3 MB (25.43%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    bool canBreak(string s, unordered_set<string>& dict, int ind, vector<int> & dp){
+        if(ind == s.size()){
+            return true;
+        }
+
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+
+        for(int i = ind + 1; i <= s.size(); i++){
+            if(dict.count(s.substr(ind, i - ind )) && canBreak(s, dict, i, dp)){
+                return dp[ind] = true;
+            }
+        }
+
+        return dp[ind] = false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        vector<int> dp(s.size() +1, -1);
+
+        return canBreak(s, dict,0, dp);
+    }
+};
