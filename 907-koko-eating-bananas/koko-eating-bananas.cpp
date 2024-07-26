ddc4bd427c13cdf9f64803aa9f379b0a3f0197e4
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-27 00:37:22 +0530

    Time: 35 ms (43.16%) | Memory: 21.7 MB (23.50%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -3,10 +3,8 @@ public:
     bool check(vector<int>& v,int mid, int h){
         double count = 0;
         for(int i = 0; i < v.size(); i++){
-            count += ceil((double)(v[i]) / (double)(mid));
-            
+            count += ceil((double)(v[i]) / (double)(mid));   
         }
-
         return count<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
@@ -14,10 +12,9 @@ public:
         int high = *max_element(piles.begin(), piles.end());
 
         while(low <= high){
-            int mid = low + (high -low) /2;
+            int mid = (low + high) /2;
             if(check(piles, mid, h)){
-                high = mid-1;
-                
+                high = mid - 1;
             }
             else{
                 low = mid + 1;
