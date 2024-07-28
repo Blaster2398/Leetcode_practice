424af59875604689ebda6166a518feea1a4e8ed3
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-07-28 12:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (51.98%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -12,12 +12,69 @@
 class Solution {
 public:
     vector<int> result;
-    vector<int> inorderTraversal(TreeNode* root) {
-        if(root != NULL){
-            inorderTraversal(root->left);
-            result.push_back(root->val);
-            inorderTraversal(root->right); 
+    vector<int> inorderTraversal1(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+
+        while(true){
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                if( st.empty() ){
+                    break;
+                }
+                node = st.top();
+                st.pop();
+
+                result.push_back(node->val);
+                node = node->right;
+            }
+
         }
-        return result;
+        return result;     
     }
+
+
+    // Morris Inorder traversal 
+    vector<int> inorderTraversal(TreeNode* root){
+        vector<int> inorder ;
+        TreeNode* curr = root;
+        while( curr != NULL ){
+            if( curr -> left == NULL ){
+                inorder.push_back(curr->val);
+                curr = curr -> right;
+            }
+            else{
+                TreeNode* prev = curr -> left;
+                while( prev->right != NULL && prev -> right != curr ) {
+                    prev = prev -> right;
+                }
+                // the thread is already made so delete it and traverse to the right 
+                if(prev -> right == curr){
+                    prev -> right = NULL;
+                    inorder.push_back(curr->val);
+                    curr = curr -> right;
+                }
+                // if the thread is not made then just make it 
+                else{
+                    prev -> right = curr;
+                    curr = curr -> left;
+                }
+            }
+        }
+        return inorder;
+    }
+
+
+    // // recursive 
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     if(root != NULL){
+    //         inorderTraversal(root->left);
+    //         result.push_back(root->val);
+    //         inorderTraversal(root->right); 
+    //     }
+    //     return result;
+    // }
 };
