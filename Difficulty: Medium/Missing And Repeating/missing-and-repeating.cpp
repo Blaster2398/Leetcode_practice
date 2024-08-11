481b69f77e9dd9d3aa02dd43d30903fe879999df
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-08-11 17:45:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-    vector<int> findTwoElement(vector<int> arr, int n) {
+    vector<int> findTwoElement1(vector<int> arr, int n) {
         vector<bool> visited(n + 1, false);
         visited[0] = true;
         vector<int> res;
@@ -27,6 +27,55 @@ public:
         }
         
         return res;
+    }
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        // long long n = arr.size();
+        int xr = 0;
+        for(int i = 0; i < n; i++){
+            xr = xr ^ arr[i];
+            xr = xr ^ (i+1);
+        }
+        int bitNum = 0;
+        while(1){
+            if((xr  & (1 << bitNum)) != 0 ){
+                break;
+            }
+            bitNum++;
+        }
+
+        int zero = 0;
+        int one = 0;
+        for(int i = 0; i  < n; i++){
+            if((arr[i] & (1 << bitNum)) != 0){
+                zero = zero ^ arr[i];
+            }
+            else{
+                one = one ^ arr[i];
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            if((i & (1 << bitNum)) != 0){
+                zero = zero ^ i;
+            }
+            else{
+                one = one ^ i;
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == zero){
+                cnt++;
+            }
+        }
+
+        if(cnt == 2){
+            return {zero, one};
+        }
+        else{
+            return {one, zero};
+        }
     }
 };
 
