ed70c9165ceffe891f50853ace9b72c32a0f1897
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-08-16 23:00:43 +0530

    Time: 21 ms (13.22%) | Memory: 18.6 MB (11.35%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -10,7 +10,7 @@ public:
 
         return dp[i][j] = max(chooseF, chooseE);
     }
-    bool stoneGame(vector<int>& piles) {
+    bool stoneGame_memo(vector<int>& piles) {
         int n = piles.size();
         vector<vector<int>> dp(n, vector<int> (n,-1));
 
@@ -19,19 +19,25 @@ public:
 
         return (alice > bob);
     }
-    // bool stoneGame(vector<int>& piles) {
-    //     int n = piles.size();
-    //     vector<vector<int>> dp(n, vector<int> (n,0));
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int> (n,0));
 
-    //     // int alice = util(piles, dp, 0, n-1);
-    //     // int bob = accumulate(piles.begin(), piles.end(), 0) - alice;
+        // int alice = util(piles, dp, 0, n-1);
 
-    //     for(int i = n-1; i >= 0; i--){
-    //         for(int j = 0; j < n; j++){
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 1; j < n; j++){
+                int chooseF = piles[i] + util(piles, dp, i+1, j);
+                int chooseE = piles[j] + util(piles, dp, i, j-1);
 
-    //         }
-    //     }
+                dp[i][j] = max(chooseF, chooseE);
+            }
+        }
 
-    //     return (alice > bob);
-    // }
+        int alice = dp[0][n-1];
+        int bob = accumulate(piles.begin(), piles.end(), 0) - alice;
+        
+
+        return (alice > bob);
+    }
 };
