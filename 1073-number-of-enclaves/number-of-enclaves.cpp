82eaef92652257d3cbf19da0019deefbe4f84975
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-08-21 23:42:22 +0530

    Time: 62 ms (21.57%) | Memory: 33.3 MB (55.53%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,61 +1,111 @@
 class Solution {
 public:
-    int BFS(vector<vector<int>> &grid, int i, int j, vector<vector<bool>> &visited, int &flag) {
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        visited[i][j] = true;
-        int cnt = 1;
+    // int BFS(vector<vector<int>> &grid, int i, int j, vector<vector<bool>> &visited, int &flag) {
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+    //     queue<pair<int, int>> q;
+    //     q.push({i, j});
+    //     visited[i][j] = true;
+    //     int cnt = 1;
 
-        // Check if the starting cell is on the boundary
-        if (i == 0 || j == 0 || i == m-1 || j == n-1) {
-            flag = 1;
-        }
+    //     if(i == 0 || j == 0 || i == m-1 || j == n-1){
+    //         flag = 1;
+    //     }
 
-        // Directions for moving in the grid (left, up, right, down)
-        vector<pair<int, int>> directions = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    //     // Directions for moving in the grid (left, up, right, down)
+    //     vector<pair<int, int>> directions = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
         
-        while (!q.empty()) {
-            auto [ci, cj] = q.front();
-            q.pop();
+    //     while (!q.empty()) {
+    //         auto [ci, cj] = q.front();
+    //         q.pop();
             
-            for (auto [di, dj] : directions) {
-                int ni = ci + di;
-                int nj = cj + dj;
+    //         for (auto [di, dj] : directions) {
+    //             int ni = ci + di;
+    //             int nj = cj + dj;
                 
-                if (ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == 1 && !visited[ni][nj]) {
-                    visited[ni][nj] = true;
-                    cnt++;
-                    q.push({ni, nj});
-                    
-                    // If the new cell is at the boundary, set the flag
-                    if (ni == 0 || ni == m-1 || nj == 0 || nj == n-1) {
-                        flag = 1;
-                    }
-                }
-            }
-        }
-        return flag == 1 ? 0 : cnt;
-    }
+    //             if (ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == 1 && !visited[ni][nj]) {
+    //                 visited[ni][nj] = true;
+    //                 cnt++;
+    //                 q.push({ni, nj});
+    //             }
+                
+    //             if ((ni == 0 || ni == m-1 || nj == 0 || nj == n-1 ) ) {  /*&& visited[ni][nj]*/
+    //                 flag = 1;
+    //             }
+    //         }
+    //     }
+    //     cout << "flag " <<flag <<endl;
+    //     return flag == 1 ? 0 : cnt;
+    // }
+
 
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         
         int count = 0;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        // vector<vector<bool>> visited(m, vector<bool>(n, false));
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == 1) {
-                    int flag = 0;
-                    int cnt = BFS(grid, i, j, visited, flag);
-                    count += cnt;
-                }
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (!visited[i][j] && grid[i][j] == 1) {
+        //             cout << "bfs called for " << i << " " << j <<endl;
+        //             int flag = 0;
+        //             int cnt = BFS(grid, i, j, visited, flag);
+        //             count += cnt;
+        //         }
+        //     }
+        // }
+
+        queue<pair<int,int>> q;
+
+        for(int i = 0; i < m; i++){
+            if(grid[i][0] == 1){
+                q.push({i,0});
+            }
+            if(grid[i][n-1] == 1){
+                q.push({i,n-1});
             }
         }
         
+        for(int i = 0; i < n; i++){
+            if(grid[0][i] == 1){
+                q.push({0,i});
+            }
+            if(grid[m-1][i] == 1){
+                q.push({m-1,i});
+            }
+        }
+
+
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            grid[i][j] = 0;
+
+            vector<pair<int,int>> dx = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+            for(auto [di,dj] : dx){
+                int ni = i + di;
+                int nj = j + dj;
+
+                if(ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == 1){
+                    grid[ni][nj] = 0;
+                    q.push({ni,nj});
+                }
+            }
+        }
+
+        for(int i = 0; i< m; i++){
+            for(int j = 0;j < n; j++){
+                if(grid[i][j] == 1){
+                    count++;
+                }
+            }
+        }
         return count;
+        
     }
 };
+
