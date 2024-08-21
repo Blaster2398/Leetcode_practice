06c4314979f7a6ca5f77c6d7ac739d67b505eaa2
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-08-21 23:42:46 +0530

    Time: 65 ms (20.38%) | Memory: 33.1 MB (55.91%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,61 +1,12 @@
 class Solution {
 public:
-    // int BFS(vector<vector<int>> &grid, int i, int j, vector<vector<bool>> &visited, int &flag) {
-    //     int m = grid.size();
-    //     int n = grid[0].size();
-    //     queue<pair<int, int>> q;
-    //     q.push({i, j});
-    //     visited[i][j] = true;
-    //     int cnt = 1;
-
-    //     if(i == 0 || j == 0 || i == m-1 || j == n-1){
-    //         flag = 1;
-    //     }
-
-    //     // Directions for moving in the grid (left, up, right, down)
-    //     vector<pair<int, int>> directions = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-        
-    //     while (!q.empty()) {
-    //         auto [ci, cj] = q.front();
-    //         q.pop();
-            
-    //         for (auto [di, dj] : directions) {
-    //             int ni = ci + di;
-    //             int nj = cj + dj;
-                
-    //             if (ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == 1 && !visited[ni][nj]) {
-    //                 visited[ni][nj] = true;
-    //                 cnt++;
-    //                 q.push({ni, nj});
-    //             }
-                
-    //             if ((ni == 0 || ni == m-1 || nj == 0 || nj == n-1 ) ) {  /*&& visited[ni][nj]*/
-    //                 flag = 1;
-    //             }
-    //         }
-    //     }
-    //     cout << "flag " <<flag <<endl;
-    //     return flag == 1 ? 0 : cnt;
-    // }
-
+   
 
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         
         int count = 0;
-        // vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (!visited[i][j] && grid[i][j] == 1) {
-        //             cout << "bfs called for " << i << " " << j <<endl;
-        //             int flag = 0;
-        //             int cnt = BFS(grid, i, j, visited, flag);
-        //             count += cnt;
-        //         }
-        //     }
-        // }
 
         queue<pair<int,int>> q;
 
