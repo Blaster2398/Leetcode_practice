533172e231b49d9d52980fedc2ccd652dac89c7c
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-08-31 12:20:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        int count = 0;
+        int num = N;
+        while(num > 0){
+            int n = num % 10;
+            num = num / 10;
+            if(n != 0 && N % n == 0) count++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
