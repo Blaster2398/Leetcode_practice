043d22840e17a75bfc56a119f86134b3e5fd94b1
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-09-07 22:26:20 +0530

    Time: 4 ms (79.97%) | Memory: 8.9 MB (82.93%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
-    bool pall(string &s, int start, int end){
-        while(start < end){
-            if(s[start] != s[end]){
+    // bool pall(string &s, int start, int end){
+    //     while(start < end){
+    //         if(s[start] != s[end]){
+    //             return false;
+    //         }
+    //         start ++;
+    //         end --;
+    //     }
+    //     return true;
+    // }
+    // bool isPalindrome(string s) {
+        
+    //     string str;
+    //     for(int i = 0; i < s.size(); i++){
+    //         if((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122) || (s[i] <= 57 && s[i] >=48)){
+    //             str+= s[i];
+    //         }
+    //     }
+    //     for (char &c : str) {
+    //         c =tolower(c);
+    //     }
+    //     cout << str <<endl;
+    //     int start = 0;
+    //     int end = str.size()-1;
+    //     return pall(str, start, end);
+    // }
+
+
+
+
+
+    bool isPalindrome(string s) {
+        int l=0;
+        int r=s.size()-1;
+
+        while(l<r){
+            while(!isalnum(s[l]) && l<r){
+                l++;
+            }
+            while(!isalnum(s[r]) && l<r){
+                r--;
+            }
+            if(tolower(s[l]) != tolower(s[r])){
                 return false;
             }
-            start ++;
-            end --;
+            l++;
+            r--;
         }
         return true;
-    }
-    bool isPalindrome(string s) {
-        
-        string str;
-        for(int i = 0; i < s.size(); i++){
-            if((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122) || (s[i] <= 57 && s[i] >=48)){
-                str+= s[i];
-            }
-        }
-        for (char &c : str) {
-            c =tolower(c);
-        }
-        cout << str <<endl;
-        int start = 0;
-        int end = str.size()-1;
-        return pall(str, start, end);
     }
 };
