8829a0af5c99df5ff11f50270dd65716fbfaff1c
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2024-12-05 14:05:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(vector<int>& arr) {
+        for(int i = 0; i < arr.size(); i++){
+            int ind = i;
+            for(int j = i + 1; j < arr.size(); j++){
+                if(arr[ind] > arr[j] ){
+                    swap(arr[ind], arr[j]);
+                    j = ind;
+                }
+            }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+
+        ob.bubbleSort(arr);
+        for (int e : arr) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
