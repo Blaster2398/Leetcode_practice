94600b9a8e384a795db7c9bb1f398a05dfb47b28
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-01-12 14:32:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (40.28%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> temp;
-        int c = 0;
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] != 0){
-                temp.push_back(nums[i]);
-            }
-            else {
-                c++;
+        int n=0;
+        int temp;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                temp = nums[i];
+                nums[i] = 0;
+                nums[n] = temp;
+                n++;
             }
         }
-
-        while(c--){
-            temp.push_back(0);
-        }
-
-        for(int i = 0; i < n; i++){
-            nums[i] = temp[i];
-        }
-
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // void moveZeroes(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> temp;
+    //     int c = 0;
+
+    //     for(int i = 0; i < n; i++){
+    //         if(nums[i] != 0){
+    //             temp.push_back(nums[i]);
+    //         }
+    //         else {
+    //             c++;
+    //         }
+    //     }
+
+    //     while(c--){
+    //         temp.push_back(0);
+    //     }
+
+    //     for(int i = 0; i < n; i++){
+    //         nums[i] = temp[i];
+    //     }
+
+    // }
 };
