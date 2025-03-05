165a46ced3b93c3242519455d1cda04e6164b5bb
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-03-05 11:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (7.10%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,10 +1,16 @@
 class Solution {
-public:
-    long long coloredCells(int n) {
-        long long res = 1;
-        for(long long i = 0; i < n; i++){
-            res = res + 4*i;
-        }
-    return res;
+// public:
+//     long long coloredCells(int n) {
+//         long long res = 1;
+//         for(long long i = 0; i < n; i++){
+//             res = res + 4*i;
+//         }
+//     return res;
+//     }
+// };
+public :    
+    long long coloredCells(int _n){
+        long long n = _n;
+        return 1 + 2 *(n) * (n-1);
     }
 };
