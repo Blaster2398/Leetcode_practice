d44bd2cbac22acb3977d4d3937ea8606e3466675
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-03-07 20:21:34 +0530

    Time: 228 ms (41.29%) | Memory: 23.4 MB (36.85%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
-vector<int> sieve(int left, int right) {
-    if (right < 2) return{}; // No primes below 2
+    vector<int> closestPrimes(int left, int right) {
 
-    // Step 1: Create an array to mark primes (0 = prime, 1 = not prime)
-    vector<int> primes(right + 1);
+        if (left > right) return {-1, -1};
 
-    for (int i = 0; i <= right; i++) {
-        primes[i] = 0; // Assume all numbers are prime initially
-    }
-
-    // Step 2: Run the sieve from 2 to sqrt(right)
-    for (int i = 2; i * i <= right; i++) {
-        if (primes[i] == 0) { // If `i` is prime
-            for (int j = i * i; j <= right; j += i) {
-                primes[j] = 1; // Mark multiples of `i` as not prime
+        // this is the sieve code 
+        vector<bool> primes(right + 1, true);
+        primes[0] = false , primes[1] = false;
+        for(long long i = 2; i <= right; i++){
+            if(!primes[i]){
+                continue;
+            }
+            else{
+                for(long long  j = i*i; j <= right; j+=i){
+                    primes[j] = false;
+                }
             }
         }
-    }
-    vector<int> result ;
-    // Step 3: Print primes from `left` to `right` in left-to-right order
-    for (int i = max(2, left); i <= right; i++) {
-        if (primes[i] == 0) {
-            cout << i << " ";
-            result.push_back(i);
-        }
-    }
 
-    return result;
-}
-    vector<int> closestPrimes(int left, int right) {
-        vector<int> primes = sieve(left, right);
+        vector<int> res;
+        for(int i = left; i < primes.size(); i++){
+            if(primes[i] == true){
+                res.push_back(i);
+            }
+        }
+
+
         int min = INT_MAX;
         int a = -1;
         int b = -1;
-        for (int i = primes.size() - 1; i > 0; i--) { 
-            if (primes[i] - primes[i - 1] <= min) {  
-                a = primes[i - 1];  
-                b = primes[i];  
-                min = primes[i] - primes[i - 1];
+
+        for(int i = res.size()-1; i > 0; i--){
+            if(res[i] - res[i-1] <= min){
+                a = res[i-1];
+                b = res[i];
+                min = res[i] - res[i-1];
             }
         }
-        return {a,b};
+        return {a, b};
     }
-    
 };
