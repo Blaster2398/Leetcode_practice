3244fb8528e1bfefd3a0c913e8a73ceec3980423
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-03-08 16:11:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (62.68%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -3,29 +3,24 @@ public:
     int minimumRecolors(string blocks, int k) {
         int st = 0;
         int en = 0;
+        int ans = 0;
         int count = 0;
-        for(en = 0;en < k; en++){
+        for(; en < k; en++){
             if(blocks[en] == 'W'){
                 count++;
             }
-            cout << "count for st = " << st << " en = " << en << " " << count <<endl;
-
         }
-        int ans = count;
-        // now we have p number of blockc that we have to colour in orer to get k cnsecutive B cells
-        
-        for(; en< blocks.size(); en++){
-            if(blocks[en] == 'W'){
-                count++;
-            }
+        ans = count;
+        for(; en < blocks.size(); en++){
             if(blocks[st] == 'W'){
                 count--;
             }
+            if(blocks[en] == 'W'){
+                count++;
+            }
             st++;
             ans = min(ans, count);
-            cout << "count for st = " << st << " en = " << en << " " << count <<endl;
         }
-
-        return ans ;
+        return ans;
     }
 };
