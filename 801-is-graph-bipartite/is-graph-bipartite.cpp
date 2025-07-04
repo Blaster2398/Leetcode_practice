e564e5fb0abd9e1927738a07092eb0ba98e167c3
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-07-04 10:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (25.85%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -13,8 +13,8 @@ private:
             
         }
     }
-    void BFS(vector<vector<int>>& graph,vector<bool> &visited,vector<int> &color, int u, bool & flag){
-        visited[u] = true;
+    void BFS(vector<vector<int>>& graph,vector<int> &color, int u, bool & flag){
+        
         color[u] = 0;
         queue<int> q;
         q.push(u);
@@ -22,46 +22,43 @@ private:
         while(!q.empty()){
             int ft = q.front();
             q.pop();
-            visited[ft] = true;
             int col = color[ft];
-
             for(auto v : graph[ft]){
-                if(visited[v] == false){
-                    visited[v] = true;
-                    color[v] = !col;
+                if(color[v] == -1){
+                    color[v] = col ^ 1;
                     q.push(v); 
                 }
                 else{
-                   if(color[v] != !col){
+                   if(color[v] == col){
                     flag = false;
-                    break;
+                    return;
                    } 
                 }
             }
         }
     }
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n, -1);
-        bool flag = true;
-        for( int i = 0; i < n; i++){
-            if(color[i] == -1){
-                DFS(graph, 0, i, flag, color);
-            }
-        }
-        return flag;
-    }
     // bool isBipartite(vector<vector<int>>& graph) {
     //     int n = graph.size();
-    //     vector<bool> visited(n , false);
-    //     vector<int> color(n, -1);  // there are 2 color 0 or 1 
+    //     vector<int> color(n, -1);
     //     bool flag = true;
     //     for( int i = 0; i < n; i++){
-    //         if(visited[i] == false){
-    //             BFS(graph, visited, color, i, flag);
+    //         if(color[i] == -1){
+    //             DFS(graph, 0, i, flag, color);
     //         }
     //     }
     //     return flag;
     // }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);  // there are 2 color 0 or 1 
+        bool flag = true;
+        for( int i = 0; i < n; i++){
+            if(color[i] == -1){
+                BFS(graph, color, i, flag);
+                if (!flag) return false;
+            }
+        }
+        return flag;
+    }
 };
