2cf3daf98df5304e2b79c91ba7688769b083bf52
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-07-15 23:00:24 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (61.65%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    // This is the memoization from n-1 -> 0 
-    int geek(int i, int j, vector<vector<int>> &dp, vector<vector<int>>& grid){
-        if(i < 0 || j < 0) return 1e9;
-        if(i == 0 && j == 0) return grid[0][0];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int cost = grid[i][j];
-    
-        int u = geek(i-1, j, dp, grid) + cost;
-        int l = geek(i, j-1, dp, grid) + cost;
-
-        return dp[i][j] = min(l,u); 
-    }
+    // this is the tabulation
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return geek(m-1, n-1, dp, grid);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[0][0];
+                else{
+                    int cost = grid[i][j];
+                    int u = ( i > 0 ) ? dp[i-1][j] + cost : INT_MAX;
+                    int l = ( j > 0 ) ? dp[i][j-1] + cost : INT_MAX;
+
+                    dp[i][j] = min(l,u); 
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
