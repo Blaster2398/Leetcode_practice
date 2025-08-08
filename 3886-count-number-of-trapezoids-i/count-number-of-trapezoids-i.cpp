60378bdc3174928fd2374f5cd82af7b6641f8704
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-08-08 18:30:39 +0530

    Time: 117 ms (45.00%) | Memory: 194 MB (79.14%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     const int mod = 1000000007;
+    using ll = long long;
     int countTrapezoids(vector<vector<int>>& points) {
-        map<int, vector<vector<int>>> levels;
+        map<ll, ll> levels;
         for(int i = 0; i < points.size(); i++){
-            levels[points[i][1]].push_back(points[i]);
+            levels[points[i][1]]++;
         }
         int total = 0;
-        map<int, long long> noOfLines;
-        for(auto &[y, vec] : levels){
-            long long size = vec.size();
+        for(auto &[y, size] : levels){
             long long lines = (size*(size-1)/2) % mod;
             total += lines;
-            noOfLines[y] = lines;
         }
 
+
         long long count = 0;
-        for(auto &[y, lines] : noOfLines){
-            count = (count + (lines*(total-lines) % mod)) % mod;
+        for(auto &[y, size] : levels){
+            long long lines = (size*(size-1)/2) % mod;
+            count = (count + (lines * (total - lines)) % mod) % mod;
             total -= lines;
         }
 
