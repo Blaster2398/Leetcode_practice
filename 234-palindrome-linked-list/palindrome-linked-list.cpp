e95a8c7150c39f2f6e12bb6091b0fcbff6597c3a
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-08-18 10:01:51 +0530

    Time: 3 ms (62.49%) | Memory: 117.8 MB (93.68%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,56 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* reverseUtil(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* nxt = head;
-        ListNode* prv = NULL;
-
-        if(head == NULL){
-            return NULL;
+    ListNode* rev(ListNode* head){
+        
+        ListNode* cur = head;
+        ListNode* pre = NULL;
+        ListNode* nxt = NULL;
+        while(cur){
+            nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
         }
-
-        while(curr != NULL){// this is impo as to ensure that the last node arrow is reversed 
-            nxt = curr-> next;
-            curr->next = prv;
-            prv = curr;
-            curr = nxt;
-        }
-
-        return prv;
+        return pre;
     }
-    ListNode* cop ( ListNode* head){
-        ListNode* cupy = new ListNode(head->val);
-        ListNode* temp2 = cupy;
-        head = head->next;
-        while(head != NULL){
-            ListNode* temp = new ListNode(head->val);
-            temp2->next = temp;
-            temp2 = temp2->next;
-            head = head->next;
-        }
-        return cupy;
-    }
-    // void print(ListNode* head){
-    //     while(head != NULL){
-    //         cout << head->val << "->";
-    //         head = head-> next;
-    //     }
-    //     cout << endl;
-    // }
     bool isPalindrome(ListNode* head) {
-        ListNode* rev = cop(head);
-        // print(rev);
-        rev = reverseUtil(rev);
-        // print(head);
-        // print(rev);
-        while(head != NULL && rev != NULL){
-            if(head->val != rev-> val){
-                return false;
-            }
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        // now heck temp and head 
+        slow = rev(slow);
+        while(slow && head){
+            if(slow->val != head->val) return false;
+            slow = slow->next;
             head = head->next;
-            rev = rev->next;
         }
         return true;
+        
     }
 };
