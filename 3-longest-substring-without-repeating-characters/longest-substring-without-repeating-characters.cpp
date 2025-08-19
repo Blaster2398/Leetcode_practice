25bd974b4448a25b12e6e6717102a6b406e78f4b
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-08-19 23:41:37 +0530

    Time: 17 ms (34.19%) | Memory: 14.3 MB (22.80%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring1(string s) {
-        
-        int res = 0;
-        int left = 0, right = 0;
-        unordered_map<char, int> st;
-
-        while(right < s.size()){
-            if(st.find(s[right]) != st.end()){
-                // either we have to update the left to the found location +1 
-                //but it can be out of range of l and r 
-                left = max(left, st[s[right]] +1);
-            }
-
-            // for each turn updarte the newly found character in the map 
-            st[s[right]] = right;
-            res = max(res, right - left + 1);
-            right ++;
-        }
-        return res;
-    }
-
-
     int lengthOfLongestSubstring(string s) {
-        
-        int res = 0;
-        int left = 0, right = 0;
-        // unordered_map<char, int> st;
-        unordered_set<int> st;
-
-        while(right < s.size()){
-            if(st.find(s[right]) != st.end()){
-                while (left < right && st.find(s[right]) != st.end()) {
-                    st.erase(s[left]);
-                    left++;
-                }
+        unordered_set<char> st;
+        int l = 0,r = 0;
+        int count = 0;
+        while(r < s.size()){
+            if(st.count(s[r]) == 0){
+                st.insert(s[r]);
+                r++;
+                count = max(count, r-l);
             }
-
-            st.insert(s[right]);
-            res = max(res, right - left + 1);
-            right ++;
+            else{
+                st.erase(s[l]);
+                l++;
+            }
         }
-        return res;
+        return count;
     }
 };
