ca6b03b945716ef62c7d93d0f492a10af23868bd
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-08-23 23:47:11 +0530

    Time: 11 ms (40.50%) | Memory: 22.7 MB (96.13%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    bool check(vector<int>& v,int mid, int h){
-        double count = 0;
-        for(int i = 0; i < v.size(); i++){
-            count += ceil((double)(v[i]) / (double)(mid));   
+    using ll = long long;
+    bool geek(vector<int>& nums, int h , int mid){
+        ll ans= 0;
+        int i = 0;
+        while(i < nums.size()){
+            ans += nums[i]/mid;
+            if(nums[i] % mid) ans++;
+            // ans += (nums[i] + mid -1) / mid;
+            i++;
         }
-        return count<=h;
+        return (ans > h) ? false : true;
     }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
-
+    int minEatingSpeed(vector<int>& nums, int h) {
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        int  ans = high;
         while(low <= high){
             int mid = (low + high) /2;
-            if(check(piles, mid, h)){
+            int check = geek(nums, h, mid);
+            if(check){
+                ans = mid;
                 high = mid - 1;
             }
             else{
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
 };
