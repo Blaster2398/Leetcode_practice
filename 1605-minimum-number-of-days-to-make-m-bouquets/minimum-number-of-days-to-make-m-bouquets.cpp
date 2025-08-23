4bee6d259eb2cf8ba2a74012953053b9b768f01c
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-08-24 00:14:00 +0530

    Time: 1325 ms (5.13%) | Memory: 70.6 MB (15.07%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    bool possible(vector<int>& bloomDay, long long int m, long long int k, long long int day) {
-        long long int cnt = 0;
-        long long int numB = 0;
-        for(long long int i = 0; i < bloomDay.size(); i++) {
-            if(bloomDay[i] <= day) {
-                cnt++;
-                if (cnt == k) {
-                    numB++;
-                    cnt = 0;
-                }
-            } else {
-                cnt = 0;
+    bool geek(vector<int>& nums, int m, int k, int mid){
+        int i = 0;
+        int ans = 0;
+        int seq = k;
+        while(i < nums.size()){
+            if(ans >= m) return true;
+
+            if(nums[i] > mid) seq = k;
+            else seq--;
+            
+            if(seq == 0){
+                cout <<" i " << i << endl;
+                ans++;
+                seq = k;
             }
+            i++;
         }
-        return numB >= m;
+        cout << "for mid " << mid << " ans is " << ans << endl;
+        return (ans >= m);
     }
-    int minDays(vector<int>& bloomDay, long long int m, long long int k) {
-        if(m*k > bloomDay.size()) return -1;
-        // ex - 7 7 7 7 12 11 13 7
-        // we know that the day we requir is between (7 , 13)  we can do a linear search for each object which 
-        // has a  req number of bouuets or we can do a binary search
-        auto it = max_element(bloomDay.begin(), bloomDay.end());
-        long long int high = *it;
-        it = min_element(bloomDay.begin(), bloomDay.end());
-        long long int low = *it;
-        long long int n = high - low + 1;
-        cout << high << " " << low << endl;
-        while(low <= high) {
-            long long int mid = (low + high)/2;
-            if(possible(bloomDay, m, k, mid)){
-                cout << mid << endl;
-                high = mid - 1;
+    int minDays(vector<int>& nums, int m, int k) {
+        if(m > nums.size()) return -1;
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        int ans  = -1;
+        while(low <= high){
+            int mid = (low + high)/2; 
+            bool check= geek(nums, m, k, mid);
+            if(check){
+                high = mid -1;
+                ans = mid;
             }
             else{
                 low = mid + 1;
             }
         }
-        return low;
-        
+        return ans ;
     }
 };
