a3f2e6f899f1e53294c232359f1069298a8b3cc7
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-08-27 00:06:44 +0530

    Time: 6 ms (28.67%) | Memory: 9.8 MB (69.12%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    string f(int n){
-        string res = "";
-        // base case 
-        if(n == 1){
-            return "1";
-        }
-        res =res + f(n-1);
-        // now do the RLE
-        int count = 1;
-        string newres = "";
-        for(int i = 0; i < res.length()-1; i++){
-            if(res[i] == res[i+1]){
-                count++;
-            }
-            else{
-                newres += to_string(count) + res[i];
-                count = 1;
-            }
-        }
-        newres += to_string(count) + res.back();
-        
-        return newres;
-    }
     string countAndSay(int n) {
-        return f(n);
+        int count = 1;
+        string s = "1";
+        while(count < n){
+            string nw = "";
+            int j = 0;
+            while(j < s.size()){
+                int i = j;
+                char num = s[j];
+                while(j < s.size() && s[j+1]== num) j++;
+                int dis = j - i + 1;
+                nw += to_string(dis) + num;
+                j++;
+            }
+            cout << nw << " string " << count << endl; 
+            s = nw;
+            count++;
+        }
+        return s;
     }
 };
