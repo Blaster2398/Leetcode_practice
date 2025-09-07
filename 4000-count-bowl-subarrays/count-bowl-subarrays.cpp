529fe29abbc1912f9ba85386c6c2289bf09a3cce
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-09-07 15:34:58 +0530

    Time: 9 ms (92.86%) | Memory: 128.2 MB (7.14%) - LeetSync

---
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long bowlSubarrays(vector<int>& nums) {
+        long long ans = 0;
+        stack<int> st;
+        for(int i = 0; i < nums.size(); i++){
+            while(!st.empty() && st.top() < nums[i]){
+                st.pop();
+                if(!st.empty()) ans++;
+            }
+            st.push(nums[i]);
+        }
+
+        return ans;
+    }
+};
