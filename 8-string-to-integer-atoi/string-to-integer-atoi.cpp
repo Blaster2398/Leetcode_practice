118d86880a5f593dac9b4e5b2ab0978a0a6257af
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-09-08 11:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (55.73%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,37 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int myAtoi(string s) {
-        int sign = 1;
-        long ans = 0;  // Use long to handle overflow during calculation
         int i = 0;
-        
-        // Skip leading spaces
-        while (i < s.length() && s[i] == ' ') {
+        long long num = 0;
+        int sign = 1;
+       
+        while( i < s.size() && s[i] == ' ') i++;
+
+        if(i < s.size() && s[i] == '-') {
+            sign = -1;
             i++;
         }
-        
-        // Handle sign
-        if (i < s.length() && (s[i] == '-' || s[i] == '+')) {
-            sign = (s[i] == '-') ? -1 : 1;
+        if(s[i] == '+' && sign != -1) i++;
+
+        while(i < s.size() && isdigit(s[i])){
+            num = 10*num + (s[i] - '0');
+            if(sign == 1 && num > INT_MAX) return INT_MAX;
+            if(sign == -1 && num > 1LL*INT_MAX + 1) return INT_MIN;
             i++;
         }
-        
-        // Convert digits
-        while (i < s.length() && isdigit(s[i])) {
-            int digit = s[i] - '0';
-            
-            // Check for overflow before updating ans
-            if (ans > (INT_MAX - digit) / 10) {
-                return (sign == 1) ? INT_MAX : INT_MIN;
-            }
-            
-            ans = ans * 10 + digit;
-            i++;
-        }
-        
-        return sign * ans;
+
+        return (int)(sign*num);
     }
 };
