d15c9d5d3ac7e9dd8ecf6ab6e4f767c358fae747
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-09-09 15:49:51 +0530

    Time: 1182 ms (23.80%) | Memory: 489.8 MB (15.39%) - LeetSync

--- 3938-twisted-mirror-path-count/twisted-mirror-path-count.cpp
+++ 3938-twisted-mirror-path-count/twisted-mirror-path-count.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int n, m;
-    int mod = 1e9 + 7;
-    int geek(int i, int j, int d, vector<vector<int>>& grid,vector<vector<vector<int>>>& dp){
-        if(i >= n || j >= m || i < 0 || j < 0)  return 0;
-        if(i == n-1 && j == m-1) return 1;
+    const int MOD = 1e9 + 7;
 
-        if(dp[i][j][d] != -1) return dp[i][j][d];
+    int dfs(int i, int j, int d, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
+        if (i < 0 || j < 0 || i >= n || j >= m) return 0;
+        if (i == n - 1 && j == m - 1) return 1;
+        if (dp[i][j][d] != -1) return dp[i][j][d];
 
-        int ways = 0;
-        if(grid[i][j] == 0) ways = ((geek(i + 1, j, 1, grid, dp) % mod) + (geek(i, j+1, 0, grid, dp) % mod)) % mod;
-        else{
-            if(d == 0) ways = (geek(i + 1, j, 1, grid, dp) % mod);
-            else ways = (geek(i, j + 1, 0, grid, dp) % mod);
+        long long ways = 0;
+        if (grid[i][j] == 0) {
+            ways += dfs(i, j + 1, 0, grid, dp);
+            ways += dfs(i + 1, j, 1, grid, dp);
+        } else {
+            if (d == 0) ways += dfs(i + 1, j, 1, grid, dp);
+            else ways += dfs(i, j + 1, 0, grid, dp);
         }
 
-        return dp[i][j][d] = ways;
+        return dp[i][j][d] = ways % MOD;
     }
+
     int uniquePaths(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-       vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(2 ,-1)));
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(2, -1)));
 
-       int count = (geek(0, 1, 0, grid, dp) % mod + geek(1, 0, 1, grid, dp) % mod) %mod;
-       return count;
+        long long ans = 0;
+        ans += dfs(0, 1, 0, grid, dp);
+        ans += dfs(1, 0, 1, grid, dp);
+
+        return ans % MOD;
     }
 };
-
