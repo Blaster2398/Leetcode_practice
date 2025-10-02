6cfe9fd9ccdc6b780522b1400738024e846aa5c4
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-10-02 23:47:16 +0530

    Time: 123 ms (57.83%) | Memory: 63 MB (8.74%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,63 +1,9 @@
 class Solution {
 public:
-    bool isPal(string s, int i, int j){
-        while(i < j){
-            if(s[i] != s[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-    int f(string s, int i, int n, vector<int>& dp){
-        if(i == n){
-            return -1;
-        }
-
-        if(dp[i] != -1) return dp[i];
-
-        int mini = INT_MAX;
-        for(int j = i; j < n; j++){
-            if(isPal(s, i, j)){
-                int count = 1 + f(s, j+1, n, dp);
-                mini = min(mini, count);
-            }
-        }
-        return dp[i] = mini;
-    }
-    int minCut_memo(string s) {
-        int n = s.size();
-        vector<int> dp(n, -1);  // only changing parameter is index i
-        return f(s, 0, n, dp);
-    }
-
-
-    // recursion 
-    int f_rec(string s, int i, int n){
-        if(i == n){
-            return -1;//this is fpr the last partition as we dont need it 
-            //ex- ABC will have A|B|C|  3 partition if we dont subtract -1 
-        }
-        int mini = INT_MAX;
-        for(int j = i; j < n; j++){
-            if(isPal(s, i, j)){
-                int count = 1 + f_rec(s, j+1, n);
-                mini = min(mini, count);
-            }
-        }
-        return mini;
-    }
-    int minCut_recursive(string s) {
-        int n = s.size();
-        return f_rec(s, 0, n);
-    }
 
     bool isPald(string& str, int s, int e, vector<vector<int>>& isPaldp) {
-        if (s >= e) return true; // Correct base case for palindrome check
-
+        if (s >= e) return true;
         if (isPaldp[s][e] != -1) return isPaldp[s][e];
-
         if (str[s] == str[e]) {
             return isPaldp[s][e] = isPald(str, s + 1, e - 1, isPaldp);
         } else {
@@ -65,31 +11,92 @@ public:
         }
     }
 
+    int f(string& s, int i, int n, vector<int>& dp, vector<vector<int>>& isPaldp) {
+        if (i == n) return -1;
+        if (dp[i] != -1) return dp[i];
+        int mini = INT_MAX;
+        for (int j = i; j < n; j++) {
+            if (isPaldp[i][j]) {
+                int count = 1 + f(s, j + 1, n, dp, isPaldp);
+                mini = min(mini, count);
+            }
+        }
+        return dp[i] = mini;
+    }
+
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n+1, INT_MAX);  // only changing parameter is index i
+        if (n <= 1) return 0;
         vector<vector<int>> isPaldp(n, vector<int>(n, -1));
-        
-        // Precompute the palindrome status
         for (int i = 0; i < n; ++i) {
             for (int j = i; j < n; ++j) {
                 isPald(s, i, j, isPaldp);
             }
         }
-
-
-        dp[n] = -1;
-        for(int i = n-1; i >= 0; i--){
-           int mini = INT_MAX;
-            for(int j = i; j < n; j++){
-                if(isPald(s, i, j, isPaldp)){
-                    int count = 1 + dp[j+1];
-                    mini = min(mini, count);
-                }
-            }
-            dp[i] = mini; 
-        }
-        return dp[0];
-
+        vector<int> dp(n + 1, -1);
+        return f(s, 0, n, dp, isPaldp);
     }
+
+
+
+
+    // recursion 
+    // int f_rec(string s, int i, int n){
+    //     if(i == n){
+    //         return -1;//this is fpr the last partition as we dont need it 
+    //         //ex- ABC will have A|B|C|  3 partition if we dont subtract -1 
+    //     }
+    //     int mini = INT_MAX;
+    //     for(int j = i; j < n; j++){
+    //         if(isPal(s, i, j)){
+    //             int count = 1 + f_rec(s, j+1, n);
+    //             mini = min(mini, count);
+    //         }
+    //     }
+    //     return mini;
+    // }
+    // int minCut_recursive(string s) {
+    //     int n = s.size();
+    //     return f_rec(s, 0, n);
+    // }
+
+    // bool isPald(string& str, int s, int e, vector<vector<int>>& isPaldp) {
+    //     if (s >= e) return true; // Correct base case for palindrome check
+
+    //     if (isPaldp[s][e] != -1) return isPaldp[s][e];
+
+    //     if (str[s] == str[e]) {
+    //         return isPaldp[s][e] = isPald(str, s + 1, e - 1, isPaldp);
+    //     } else {
+    //         return isPaldp[s][e] = false;
+    //     }
+    // }
+
+    // int minCut(string s) {
+    //     int n = s.size();
+    //     vector<int> dp(n+1, INT_MAX);  // only changing parameter is index i
+    //     vector<vector<int>> isPaldp(n, vector<int>(n, -1));
+        
+    //     // Precompute the palindrome status
+    //     for (int i = 0; i < n; ++i) {
+    //         for (int j = i; j < n; ++j) {
+    //             isPald(s, i, j, isPaldp);
+    //         }
+    //     }
+
+
+    //     dp[n] = -1;
+    //     for(int i = n-1; i >= 0; i--){
+    //        int mini = INT_MAX;
+    //         for(int j = i; j < n; j++){
+    //             if(isPald(s, i, j, isPaldp)){
+    //                 int count = 1 + dp[j+1];
+    //                 mini = min(mini, count);
+    //             }
+    //         }
+    //         dp[i] = mini; 
+    //     }
+    //     return dp[0];
+
+    // }
 };
