3c41730b6888777ccf9523522bf0677b10cc5012
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-10-05 15:51:41 +0530

    Time: 1 ms (22.50%) | Memory: 8.8 MB (59.29%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> freq (26, 0);
+        vector<int> vis (26, 0);
+        for(char c : s) freq[c-'a']++;
+
+        stack<char> st;
+
+        for(int i = 0; i < s.size(); i++){
+            char c = s[i];
+            if(vis[c-'a']){
+                freq[c-'a']--;
+                continue;
+            }
+            while(!st.empty() && freq[st.top() - 'a'] > 1 && c < st.top()){
+                freq[st.top() -'a']--;
+                vis[st.top() - 'a'] = 0;
+                st.pop();
+            }
+            st.push(c);
+            vis[c-'a'] = 1;
+        }
+
+        string res = "";
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+
+
+    }
+};
