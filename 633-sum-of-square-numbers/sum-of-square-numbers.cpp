848e33bb7dee50a51c793cdb194addfe5ffe87e6
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-10-11 16:11:42 +0530

    Time: 2 ms (47.82%) | Memory: 7.8 MB (54.15%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     bool judgeSquareSum(int c) {
         for(int a = 0; a <= sqrt(c); a++){
-            double b = sqrt(c-a*a);
-            if(b == (int)b){
-                cout << a << " " << b<< endl;
-                return true;
-            }
+            int b = sqrt(c - a*a);
+            if(b*b == c - a*a) return true;
         }
         return false;
     }
