69b62de2089d278d9d1139e2163f8b18ccd041bd
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-10-11 17:27:36 +0530

    Time: 39 ms (84.65%) | Memory: 13.3 MB (59.07%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    int geek(int ind, int target, int n, vector<int>& arr, vector<vector<int>>& dp)
-    {
-        if(target == 0) return 0;
-        if(ind >= n) return 1e8;
-
-        if(dp[ind][target] != -1) return dp[ind][target];
-
-        int ntake = geek(ind+1, target, n, arr, dp);
-        int take = (target >= arr[ind]) ? 1 + geek(ind, target - arr[ind], n, arr, dp) : INT_MAX;
-        return dp[ind][target] = min(take, ntake);
-    }
     int numSquares(int n) {
-        vector<int> squares;
-        for(int i = 1; i <= sqrt(n); i++){
-            squares.push_back(i*i);
+        vector<int> dp(n+1, 1e9);
+        dp[0] = 0;
+        for(int i = 1; i*i <= n; i++){
+            int sq = i*i;
+           for(int j = sq; j <= n; j++){
+            dp[j] = min(dp[j] , dp[j-sq] +1);
+           }
         }
-
-        // now this is target sum from dp 
-        int n1 = squares.size();
-        vector<vector<int>> dp(n1+1, vector<int>(n+1, -1));
-        return geek(0, n, n1, squares, dp);
+        return dp[n];
     }
 };
