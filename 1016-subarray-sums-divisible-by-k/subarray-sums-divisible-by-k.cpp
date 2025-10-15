e00b5ce12d46ba60448d4e8dbab87b45a7ad7dcc
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-10-15 17:35:13 +0530

    Time: 32 ms (7.11%) | Memory: 36 MB (12.32%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        // the concept is firsly we find the prefix array 
+        // and then take the modulo k so that if the modulo repeats 
+        // we can say that the prefix sum / sum bw those 
+        // indexes might have been divisible by k
+        map<int,int> mp;
+        mp[0] = 1;
+        int sum  =0;
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            int mod = ((sum % k) + k) % k;
+            if(mp.count(mod)) ans += mp[mod];
+            mp[mod]++;
+        }
+
+        return ans;
+        
+    }
+};
