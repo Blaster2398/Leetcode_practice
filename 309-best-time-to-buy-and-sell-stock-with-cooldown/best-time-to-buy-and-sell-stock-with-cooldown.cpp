b9904f8eb07ae398bab8ef60e66e646dfa8c2066
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-10-16 15:36:09 +0530

    Time: 4 ms (21.35%) | Memory: 16.5 MB (10.59%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,73 +1,27 @@
 class Solution {
 public:
-    // recursive + memo
-    int util(vector<int>& prices, int ind, int buy,vector<vector<int>>& dp){
-        // base cases 
-        if(ind >= prices.size()){
-            return 0;
+    vector<vector<int>> dp;
+    vector<int> p;
+    int geek(int i, int sell){
+        if(i >= p.size()) return 0;
+
+        if(dp[i][sell] != -1) return dp[i][sell];
+
+        int ways = 0;
+        if(sell == 0){
+            // we can buy
+            ways = max(geek(i+1, 0) , -p[i] + geek(i+1, 1));
         }
-        if(dp[ind][buy] != -1){
-            return dp[ind][buy];
-        }
-        // steps
-        int profit = 0;
-        if(buy){
-            profit = max(-prices[ind]+ util(prices, ind+1, 0, dp), 0 + util(prices, ind+1, 1, dp));
-        } else {
-            profit = max(prices[ind] + util(prices, ind+2, 1, dp), 0 + util(prices, ind+1, 0, dp));
+        else{
+            // we can sell
+            ways = max(geek(i+1, 1) , p[i] + geek(i+2, 0));
         }
 
-        return dp[ind][buy] = profit;
+        return dp[i][sell] = ways;
     }
-    int maxProfit_memo(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
-        return util(prices, 0, 1, dp);
-    }
-
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> front1(2,0), curr(2,0), front2(2,0);
-
-        // //base case
-        // front1[0] = front1[1] = 0;
-
-        // as it is top doun go from n-1 to 0
-        for(int ind = n-1; ind >= 0; ind--){
-            for(int buy = 0; buy <=1; buy++){
-                int profit = 0;
-                if(buy){
-                    profit = max(-prices[ind]+ front1[0], 0 + front1[1]);
-                } else {
-                    profit = max(prices[ind] + front2[1], 0 + front1[0]);
-                }
-                curr[buy] = profit;
-            }
-            front2 = front1;
-            front1 = curr;
-        }
-        return curr[1];
-    }
-
-    // TAbulatin
-    int maxProfit_tab(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2,0));
-
-        //base case
-        dp[n][0] = dp[n][1] = 0;
-
-        // as it is top doun go from n-1 to 0
-        for(int ind = n-1; ind >= 0; ind--){
-            for(int buy = 0; buy <=1; buy++){
-                int profit = 0;
-                if(buy){
-                    profit = max(-prices[ind]+ dp[ind+1][0], 0 + dp[ind+1][1]);
-                } else {
-                    profit = max(prices[ind] +((ind+2 >= n) ? 0 : dp[ind+2][1]), 0 + dp[ind+1][0]);
-                }
-                dp[ind][buy] = profit;
-            }
-        }
-        return dp[0][1];
+        p = prices;
+        dp.assign(p.size(), vector<int>(2, -1));
+        return geek(0, 0);
     }
 };
