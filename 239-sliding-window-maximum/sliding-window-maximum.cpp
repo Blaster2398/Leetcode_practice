5d75313a164d5b939590d04275a2762792129656
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-10-16 15:09:34 +0530

    Time: 27 ms (46.57%) | Memory: 139 MB (93.18%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        // for a deque we can push_back, push_front, pop_back , pop_front 
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> dq;
-        vector<int>res;
-        for(int i = 0; i < arr.size(); i++){
-            if(dq.empty()){
-                dq.push_back(i);
-            }
-            else{
-                if(!dq.empty() && dq.front() < i - k + 1) {
-                    dq.pop_front();
-                }
-                while(!dq.empty() && arr[dq.back()] <= arr[i] ){
-                    dq.pop_back();
-                }
-                dq.push_back(i);
-
-            }
-            if(i >= k-1)  res.push_back(arr[dq.front()]);
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++){
+            // check for any removals from the front 
+            while(!dq.empty() && dq.front() + k <= i) dq.pop_front();
+            while(!dq.empty() && nums[dq.back()] < nums[i])dq.pop_back();
+            dq.push_back(i);
+            if(i >= k-1) res.push_back(nums[dq.front()]);
         }
         return res;
     }
