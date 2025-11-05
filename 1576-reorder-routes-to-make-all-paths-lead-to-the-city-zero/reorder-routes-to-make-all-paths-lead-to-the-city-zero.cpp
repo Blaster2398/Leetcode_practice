d75bff0cab6b60a99e1145a633db6d511312c0ad
Aryan Patel <133494008+Blaster2398@users.noreply.github.com>
2025-11-05 21:44:12 +0530

    Time: 89 ms (81.75%) | Memory: 105.8 MB (82.24%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+    vector<vector<pair<int,int>>> adj(n);
+    for (auto& e : connections) {
+        int u = e[0], v = e[1];
+        adj[u].push_back({v, 1});
+        adj[v].push_back({u, 0});
+    }
+    queue<int> q;
+    vector<int> vis(n, 0);
+    q.push(0);
+    vis[0] = 1;
     int ans = 0;
-    vector<bool> vis;
-    vector<vector<int>> adj;
-    void dfs(int u){
-        vis[u] = true;
-        for(auto v : adj[u]){
-            if(!vis[abs(v)]){
-                ans += (v > 0);
-                dfs(abs(v));
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (auto& [v, cost] : adj[u]) {
+            if (!vis[v]) {
+                vis[v] = 1;
+                ans += cost;
+                q.push(v);
             }
         }
-    } 
-    int minReorder(int n, vector<vector<int>>& connections) {
-        // we will do a dfs from the root node 0 to all the nodes that can be reached
-        adj.assign(n, {});
-        for(int i = 0; i < connections.size(); i++){
-            int u = connections[i][0];
-            int v = connections[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(-u);
-        }
-        vis.assign(n, false);
-        dfs(0);
-        return ans;
     }
+    return ans;
+}
+
 };
+
